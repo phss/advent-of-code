7c558c4f160a8e6177355151ba24276ead07ab3d
Paulo Schneider <paulo@vw-dilab.com>
2022-01-03 10:26:52 +0000

    Solve day 2 part 1 in rust

--- 2021/rust/src/day2.rs
+++ 2021/rust/src/day2.rs
@@ -30,18 +30,56 @@ impl FromStr for Command {
     }
 }
 
-pub fn part1() -> u32 {
-    let commands: Vec<Command> = parser::read("data/temp.txt").unwrap();
-    println!("{:?}", commands);
-    0
+#[derive(Debug, PartialEq)]
+struct Location {
+    horizontal_position: u32,
+    depth: u32,
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+pub fn part1() -> u32 {
+    let commands: Vec<Command> = parser::read("data/day2.txt").unwrap();
+    let location = estimate_location(commands);
+    location.horizontal_position * location.depth
+}
 
-//     #[test]
-//     fn blah() {
-//         assert_eq!(1, 1);
-//     }
-// }
+fn estimate_location(commands: Vec<Command>) -> Location {
+    let mut location = Location {
+        horizontal_position: 0,
+        depth: 0,
+    };
+
+    for command in commands {
+        match command {
+            Command::Forward(value) => location.horizontal_position += value,
+            Command::Down(value) => location.depth += value,
+            Command::Up(value) => location.depth -= value,
+        }
+    }
+
+    location
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_input_estimated_position() {
+        let commands = vec![
+            Command::Forward(5),
+            Command::Down(5),
+            Command::Forward(8),
+            Command::Up(3),
+            Command::Down(8),
+            Command::Forward(2),
+        ];
+
+        assert_eq!(
+            estimate_location(commands),
+            Location {
+                horizontal_position: 15,
+                depth: 10
+            }
+        );
+    }
+}
