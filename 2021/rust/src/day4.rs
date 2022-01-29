7c37f23d75427f37bb8d546f9b100ead2729776f
Paulo Schneider <paulo.schneider@gmail.com>
2022-01-29 15:28:25 +0000

    Parse numbers from input

--- 2021/rust/src/day4.rs
+++ 2021/rust/src/day4.rs
@@ -1,3 +1,28 @@
+use crate::parser;
+
+type Board = Vec<Vec<u32>>;
+
+#[derive(Debug)]
+struct Input {
+    numbers: Vec<u32>,
+    boards: Vec<Board>,
+}
+
+fn parse(lines: &Vec<String>) -> Input {
+    let numbers: Vec<u32> = lines
+        .first()
+        .unwrap()
+        .split(",")
+        .map(|n| n.parse().unwrap())
+        .collect();
+    Input {
+        numbers,
+        boards: vec![],
+    }
+}
+
 pub fn part1() -> u32 {
+    let lines: Vec<String> = parser::read("data/temp.txt").unwrap();
+    println!("{:?}", parse(&lines));
     0
 }
