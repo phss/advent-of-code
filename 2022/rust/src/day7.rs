b930ca67d49f8eacd8dd7668a43c5029753ef7fc
Paulo <paulo@vw-dilab.com>
2022-12-07 14:43:44 +0000

    Actually parse the commands as a directory tree

--- 2022/rust/src/day7.rs
+++ 2022/rust/src/day7.rs
@@ -1,6 +1,18 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, slice::Iter};
 
 use crate::parser;
+
+#[derive(PartialEq, Debug)]
+enum DirTree {
+    Dir {
+        name: String,
+        children: Vec<DirTree>,
+    },
+    File {
+        name: String,
+        size: u32,
+    },
+}
 
 #[derive(Clone, Debug)]
 struct DirSizes {
@@ -75,9 +87,132 @@ fn parse_to_dir_sizes(command_results: &Vec<String>) -> Vec<DirSizes> {
     dirs.values().cloned().collect()
 }
 
+fn parse(command_results: &mut Iter<String>) -> DirTree {
+    command_results.next(); // skip the first $ cd /
+    parse_in_dir("/".to_string(), command_results)
+}
+
+fn parse_in_dir(name: String, command_results: &mut Iter<String>) -> DirTree {
+    let mut children: Vec<DirTree> = vec![];
+
+    while let Some(line) = command_results.next() {
+        let parts: Vec<&str> = line.split_ascii_whitespace().collect();
+
+        match parts[..] {
+            ["$", "ls"] | ["dir", _] => {}
+            ["$", "cd", ".."] => {
+                break;
+            }
+            ["$", "cd", name] => {
+                children.push(parse_in_dir(name.to_string(), command_results));
+            }
+            [size, name] => children.push(DirTree::File {
+                name: name.to_string(),
+                size: size.parse().unwrap(),
+            }),
+            _ => println!("unreachable"),
+        }
+    }
+
+    DirTree::Dir { name, children }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn sample_input_parsing() {
+        let command_results = vec![
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "dir d",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+            "$ cd d",
+            "$ ls",
+            "4060174 j",
+            "8033020 d.log",
+            "5626152 d.ext",
+            "7214296 k",
+        ];
+        let mut command_results: Vec<String> =
+            command_results.into_iter().map(|s| s.to_string()).collect();
+
+        assert_eq!(
+            parse(&mut command_results.iter()),
+            DirTree::Dir {
+                name: "/".to_string(),
+                children: vec![
+                    DirTree::File {
+                        name: "b.txt".to_string(),
+                        size: 14848514
+                    },
+                    DirTree::File {
+                        name: "c.dat".to_string(),
+                        size: 8504156
+                    },
+                    DirTree::Dir {
+                        name: "a".to_string(),
+                        children: vec![
+                            DirTree::File {
+                                name: "f".to_string(),
+                                size: 29116
+                            },
+                            DirTree::File {
+                                name: "g".to_string(),
+                                size: 2557
+                            },
+                            DirTree::File {
+                                name: "h.lst".to_string(),
+                                size: 62596
+                            },
+                            DirTree::Dir {
+                                name: "e".to_string(),
+                                children: vec![DirTree::File {
+                                    name: "i".to_string(),
+                                    size: 584
+                                },]
+                            },
+                        ]
+                    },
+                    DirTree::Dir {
+                        name: "d".to_string(),
+                        children: vec![
+                            DirTree::File {
+                                name: "j".to_string(),
+                                size: 4060174
+                            },
+                            DirTree::File {
+                                name: "d.log".to_string(),
+                                size: 8033020
+                            },
+                            DirTree::File {
+                                name: "d.ext".to_string(),
+                                size: 5626152
+                            },
+                            DirTree::File {
+                                name: "k".to_string(),
+                                size: 7214296
+                            },
+                        ]
+                    },
+                ]
+            }
+        );
+    }
 
     #[test]
     fn sample_input_marker_index() {
