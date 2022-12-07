98873d1644949711d87a1bf0cfa4c43129e565bf
Paulo <paulo@vw-dilab.com>
2022-12-07 13:54:31 +0000

    Terrible solution to day 7 part 2

--- 2022/rust/src/day7.rs
+++ 2022/rust/src/day7.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 
 use crate::parser;
 
-
 #[derive(Clone, Debug)]
 struct DirSizes {
     size: u32,
@@ -14,7 +13,8 @@ pub fn part1() -> u32 {
 }
 
 pub fn part2() -> u32 {
-    0
+    let command_results: Vec<String> = parser::read("data/day7.txt").unwrap();
+    smallest_delete(&command_results)
 }
 
 fn sub_10000_dir_sizes(command_results: &Vec<String>) -> u32 {
@@ -24,6 +24,20 @@ fn sub_10000_dir_sizes(command_results: &Vec<String>) -> u32 {
         .filter(|dir| dir.size <= 100000)
         .map(|dir| dir.size)
         .sum()
+}
+
+fn smallest_delete(command_results: &Vec<String>) -> u32 {
+    let dirs: Vec<DirSizes> = parse_to_dir_sizes(command_results);
+    let sizes: Vec<u32> = dirs.into_iter().map(|dir| dir.size).collect();
+    let root_size = sizes.iter().max().unwrap();
+    let remaining = 70000000 - root_size;
+    let to_delete = 30000000 - remaining;
+
+    sizes
+        .into_iter()
+        .filter(|&size| size > to_delete)
+        .min()
+        .unwrap()
 }
 
 fn parse_to_dir_sizes(command_results: &Vec<String>) -> Vec<DirSizes> {
@@ -39,17 +53,21 @@ fn parse_to_dir_sizes(command_results: &Vec<String>) -> Vec<DirSizes> {
                 current_branch.pop();
             }
             ["$", "cd", dir] => {
-                let path = if let Some(path) = current_branch.last() { path } else { "" };
+                let path = if let Some(path) = current_branch.last() {
+                    path
+                } else {
+                    ""
+                };
                 let dir_path = format!("{}/{}", path, dir);
                 current_branch.push(format!("{}/{}", path, dir));
                 dirs.insert(dir_path, DirSizes { size: 0 });
-            },
+            }
             [size, _] => {
                 let size: u32 = size.parse().unwrap();
                 for dir_path in current_branch.iter().cloned() {
                     dirs.get_mut(&dir_path).unwrap().size += size;
                 }
-            },
+            }
             _ => println!("unreachable"),
         }
     }
@@ -88,7 +106,40 @@ mod tests {
             "5626152 d.ext",
             "7214296 k",
         ];
-        let command_results: Vec<String> = command_results.into_iter().map(|s| s.to_string()).collect();
+        let command_results: Vec<String> =
+            command_results.into_iter().map(|s| s.to_string()).collect();
         assert_eq!(sub_10000_dir_sizes(&command_results), 95437);
+    }
+
+    #[test]
+    fn sample_input_smallest_delete() {
+        let command_results = vec![
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "dir d",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+            "$ cd d",
+            "$ ls",
+            "4060174 j",
+            "8033020 d.log",
+            "5626152 d.ext",
+            "7214296 k",
+        ];
+        let command_results: Vec<String> =
+            command_results.into_iter().map(|s| s.to_string()).collect();
+        assert_eq!(smallest_delete(&command_results), 24933642);
     }
 }
