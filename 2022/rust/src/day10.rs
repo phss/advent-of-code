d88d4c6ccf05424335b0b2ec208ac85cbbc2430a
Paulo <paulo@vw-dilab.com>
2022-12-14 09:17:20 +0000

    Solution for day 10 part 1

---
+++ 2022/rust/src/day10.rs
@@ -0,0 +1,88 @@
+use std::{str::FromStr, vec};
+
+use crate::parser;
+
+#[derive(Debug, Clone)]
+enum Instruction {
+    Noop,
+    Addx(i32),
+}
+
+#[derive(Debug, Clone)]
+struct ParseInstructionError;
+
+impl FromStr for Instruction {
+    type Err = ParseInstructionError;
+
+    fn from_str(s: &str) -> Result<Self, ParseInstructionError> {
+        let mut splitted_line = s.split_whitespace();
+        let instruction = splitted_line.next().unwrap();
+        match instruction {
+            "noop" => Ok(Instruction::Noop),
+            "addx" => Ok(Instruction::Addx(
+                splitted_line.next().map(|v| v.parse().unwrap()).unwrap(),
+            )),
+            _ => Err(ParseInstructionError),
+        }
+    }
+}
+
+pub fn part1() -> u32 {
+    let program: Vec<Instruction> = parser::read("data/day10.txt").unwrap();
+    strength_signal_sum(&program) as u32
+}
+
+pub fn part2() -> u32 {
+    0
+}
+
+fn strength_signal_sum(program: &Vec<Instruction>) -> i32 {
+    execution_register(program)
+        .iter()
+        .enumerate()
+        .skip(20)
+        .step_by(40)
+        .map(|(i, v)| i as i32 * v)
+        .sum()
+}
+
+fn execution_register(program: &Vec<Instruction>) -> Vec<i32> {
+    let mut register: Vec<i32> = vec![];
+    let mut x = 1;
+    register.push(x);
+
+    for instruction in program {
+        register.push(x);
+
+        match instruction {
+            Instruction::Addx(value) => {
+                register.push(x);
+                x += value
+            }
+            Instruction::Noop => {}
+        }
+    }
+
+    register
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::day10::Instruction::*;
+
+    #[test]
+    fn execution_cycles_output() {
+        let program = vec![Noop, Addx(3), Addx(-5)];
+        assert_eq!(execution_register(&program), vec![1, 1, 1, 1, 4, 4]);
+    }
+
+    #[test]
+    fn sample_input_part_1() {
+        let program: Vec<Instruction> = parser::read("data/day10test.txt").unwrap();
+        assert_eq!(strength_signal_sum(&program), 13140);
+    }
+
+    #[test]
+    fn sample_input_part_2() {}
+}
--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,4 +1,4 @@
-use advent_of_code::{day1, day12, day2, day3, day4, day5, day6, day7, day8, day9, day13};
+use advent_of_code::{day1, day12, day2, day3, day4, day5, day6, day7, day8, day9, day13, day10};
 use std::env;
 
 fn parse_command_line_args() -> (u32, u32) {
@@ -34,6 +34,8 @@ fn run(day: u32, part: u32) -> u32 {
         (8, 2) => day8::part2,
         (9, 1) => day9::part1,
         (9, 2) => day9::part2,
+        (10, 1) => day10::part1,
+        (10, 2) => day10::part2,
         (12, 1) => day12::part1,
         (12, 2) => day12::part2,
         (13, 1) => day13::part1,
