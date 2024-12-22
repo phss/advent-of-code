2ad18eb470397ffec0f5c46e8a975f1d82b501fe
Paulo Schneider <paulo.schneider@gmail.com>
2024-12-22 15:53:30 +0000

    Solution for day 3 part 2

--- 2024/rust/src/day3.rs
+++ 2024/rust/src/day3.rs
@@ -1,6 +1,15 @@
+use core::panic;
+
 use regex::Regex;
 
 use crate::parser;
+
+#[derive(Debug, Clone)]
+enum Instruction {
+    Mult(u32, u32),
+    Do,
+    DoNot,
+}
 
 pub fn part1() -> u32 {
     let memory: Vec<String> = parser::read("data/day3.txt").unwrap();
@@ -8,22 +17,63 @@ pub fn part1() -> u32 {
 }
 
 pub fn part2() -> u32 {
-    0
+    let memory: Vec<String> = parser::read("data/day3.txt").unwrap();
+    mult_corrupted_with_flags(&memory)
 }
 
 fn mult_corrupted(memory: &Vec<String>) -> u32 {
-    let mul_regexp = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let mut result = 0;
 
-    for line in memory {
-        for capture in mul_regexp.captures_iter(line) {
-            let x: u32 = capture[1].parse().unwrap();
-            let y: u32 = capture[2].parse().unwrap();
+    for instruction in parse_instructions(memory) {
+        if let Instruction::Mult(x, y) = instruction {
             result += x * y;
         }
     }
 
     result
+}
+
+fn mult_corrupted_with_flags(memory: &Vec<String>) -> u32 {
+    let mut enabled = true;
+    let mut result = 0;
+
+    for instruction in parse_instructions(memory) {
+        match instruction {
+            Instruction::Mult(x, y) => {
+                if enabled {
+                    result += x * y;
+                }
+            }
+            Instruction::Do => enabled = true,
+            Instruction::DoNot => enabled = false,
+        }
+    }
+
+    result
+}
+
+fn parse_instructions(memory: &Vec<String>) -> Vec<Instruction> {
+    let instructions_regexp = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+    let mut instructions = Vec::new();
+
+    for line in memory {
+        for capture in instructions_regexp.captures_iter(line) {
+            let operation = capture.get(0).unwrap().as_str().split('(').next().unwrap();
+            let instruction = match &operation {
+                &"mul" => {
+                    let x: u32 = capture[1].parse().unwrap();
+                    let y: u32 = capture[2].parse().unwrap();
+                    Instruction::Mult(x, y)
+                }
+                &"do" => Instruction::Do,
+                &"don't" => Instruction::DoNot,
+                invalid => panic!("Invalid instruction {}", invalid),
+            };
+            instructions.push(instruction);
+        }
+    }
+
+    instructions
 }
 
 #[cfg(test)]
@@ -42,5 +92,13 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let memory =
+            vec!["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"];
+        let memory: Vec<String> = memory.into_iter().map(|s| s.to_string()).collect();
+
+        let result = mult_corrupted_with_flags(&memory);
+
+        assert_eq!(result, 48);
+    }
 }
