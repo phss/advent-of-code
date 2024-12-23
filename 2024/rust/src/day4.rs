0e74019767adb5559450e2305bbee15c70e4f49e
Paulo Schneider <paulo.schneider@gmail.com>
2024-12-23 15:25:15 +0000

    Solution for day 4 part 2

--- 2024/rust/src/day4.rs
+++ 2024/rust/src/day4.rs
@@ -16,7 +16,8 @@ pub fn part1() -> u32 {
 }
 
 pub fn part2() -> u32 {
-    0
+    let memory: Vec<String> = parser::read("data/day4.txt").unwrap();
+    x_mas_count(&memory)
 }
 
 fn xmas_count(word_search: &Vec<String>) -> u32 {
@@ -41,6 +42,35 @@ fn xmas_count(word_search: &Vec<String>) -> u32 {
                     if is_word_in_direction(word_search, "XMAS", (x, y), *direction) {
                         count += 1;
                     }
+                }
+            }
+        }
+    }
+
+    count
+}
+
+fn x_mas_count(word_search: &Vec<String>) -> u32 {
+    let width = word_search[0].len();
+    let height = word_search.len();
+    let mut count = 0;
+
+    for y in 1..height - 1 {
+        for x in 1..width - 1 {
+            if Some('A') == word_search.char_at(x, y) {
+                let top_left_down_mas =
+                    is_word_in_direction(word_search, "MAS", (x - 1, y - 1), (1, 1));
+                let top_right_down_mas =
+                    is_word_in_direction(word_search, "MAS", (x + 1, y - 1), (-1, 1));
+                let top_left_down_sam =
+                    is_word_in_direction(word_search, "SAM", (x - 1, y - 1), (1, 1));
+                let top_right_down_sam =
+                    is_word_in_direction(word_search, "SAM", (x + 1, y - 1), (-1, 1));
+
+                if (top_left_down_mas || top_left_down_sam)
+                    && (top_right_down_mas || top_right_down_sam)
+                {
+                    count += 1;
                 }
             }
         }
@@ -96,5 +126,23 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let word_search = vec![
+            "MMMSXXMASM",
+            "MSAMXMSMSA",
+            "AMXSXMAAMM",
+            "MSAMASMSMX",
+            "XMASAMXAMM",
+            "XXAMMXXAMA",
+            "SMSMSASXSS",
+            "SAXAMASAAA",
+            "MAMMMXMMMM",
+            "MXMXAXMASX",
+        ];
+        let word_search: Vec<String> = word_search.into_iter().map(|s| s.to_string()).collect();
+
+        let result = x_mas_count(&word_search);
+
+        assert_eq!(result, 9)
+    }
 }
