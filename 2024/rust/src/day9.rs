6dc8c14db8c54dba19e69095b6629ba6e1d9e82c
Paulo Schneider <paulo.schneider@gmail.com>
2024-12-28 15:02:28 +0000

    Solution for day 9 part 2

--- 2024/rust/src/day9.rs
+++ 2024/rust/src/day9.rs
@@ -1,5 +1,3 @@
-use std::{collections::VecDeque, ops::Neg};
-
 use itertools::Itertools;
 use num_bigint::BigInt;
 
@@ -31,55 +29,83 @@ fn parse_disk_map(lines: Vec<String>) -> Vec<u32> {
 }
 
 fn compact_checksum(disk_map: &mut Vec<u32>) -> BigInt {
-    let mut disk_alloc = into_disk_alloc(disk_map);
+    let mut array = into_disk_array(disk_map);
     let mut front_idx = 0;
-    let mut back_idx = disk_alloc.len() - 1;
+    let mut back_idx = array.len() - 1;
 
     loop {
-        while disk_alloc[front_idx] != EMPTY {
+        while array[front_idx] != EMPTY {
             front_idx += 1;
         }
-        while disk_alloc[back_idx] == EMPTY {
+        while array[back_idx] == EMPTY {
             back_idx -= 1;
         }
         if front_idx >= back_idx {
             break;
         }
 
-        disk_alloc.swap(front_idx, back_idx);
+        array.swap(front_idx, back_idx);
     }
 
-    checksum(&disk_alloc)
+    checksum(&array)
 }
 
 fn whole_file_compact_checksum(disk_map: &mut Vec<u32>) -> BigInt {
-    // let mut disk_alloc = VecDeque::from(into_disk_alloc(disk_map));
-    // let mut compact_alloc = Vec::new();
+    let mut array = into_disk_array(disk_map);
+    let (file_locs, mut empty_locs) = extract_locs(disk_map);
 
-    // let mut front_node = disk_alloc.pop_front();
-    // let mut back_node = disk_alloc.pop_back();
+    for (file_size, file_idx) in file_locs.iter().rev() {
+        let first_empty = empty_locs.iter().find_position(|(empty_size, empty_idx)| {
+            empty_size >= file_size && empty_idx < file_idx
+        });
 
-    // checksum(&disk_alloc)
-    0.into()
+        if let Some((loc_idx, (_, empty_idx))) = first_empty {
+            (0..*file_size).for_each(|i| {
+                let i = i as usize;
+                array.swap(empty_idx + i, file_idx + i);
+            });
+
+            empty_locs[loc_idx].0 -= file_size;
+            empty_locs[loc_idx].1 += *file_size as usize;
+        }
+    }
+
+    checksum(&array)
 }
 
-fn into_disk_alloc(disk_map: &mut Vec<u32>) -> Vec<i32> {
+fn into_disk_array(disk_map: &mut Vec<u32>) -> Vec<i32> {
     let size: u32 = disk_map.iter().sum();
-    let mut disk_alloc = vec![EMPTY; size as usize];
-    let mut alloc_id = 0;
+    let mut array = vec![EMPTY; size as usize];
+    let mut array_idx = 0;
 
     disk_map.push(0);
     for (idx, (file_size, free_space_size)) in disk_map.iter().tuples().enumerate() {
         let id = idx as i32;
 
         (0..*file_size).for_each(|_| {
-            disk_alloc[alloc_id] = id;
-            alloc_id += 1;
+            array[array_idx] = id;
+            array_idx += 1;
         });
-        alloc_id += *free_space_size as usize;
+        array_idx += *free_space_size as usize;
     }
 
-    disk_alloc
+    array
+}
+
+fn extract_locs(disk_map: &mut Vec<u32>) -> (Vec<(u32, usize)>, Vec<(u32, usize)>) {
+    let mut files = Vec::new();
+    let mut empties = Vec::new();
+    let mut array_idx = 0;
+
+    for (file_size, free_space_size) in disk_map.iter().tuples() {
+        files.push((*file_size, array_idx));
+        array_idx += *file_size as usize;
+
+        empties.push((*free_space_size, array_idx));
+        array_idx += *free_space_size as usize;
+    }
+
+    (files, empties)
 }
 
 fn checksum(disk_alloc: &Vec<i32>) -> BigInt {
@@ -87,7 +113,7 @@ fn checksum(disk_alloc: &Vec<i32>) -> BigInt {
 
     for (idx, id) in disk_alloc.iter().enumerate() {
         if *id >= 0 {
-            checksum += *id as u32 * idx as u32
+            checksum += *id as u32 * idx as u32;
         }
     }
 
@@ -113,6 +139,6 @@ mod tests {
 
         let result = whole_file_compact_checksum(&mut disk_map);
 
-        // assert_eq!(result, 2858.into())
+        assert_eq!(result, 2858.into())
     }
 }
