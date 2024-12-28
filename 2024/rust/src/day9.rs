d64c00034e1218d0577d378837e7a6842b46daf7
Paulo Schneider <paulo.schneider@gmail.com>
2024-12-28 13:38:43 +0000

    Simplify day 9 solution by using a memory array

--- 2024/rust/src/day9.rs
+++ 2024/rust/src/day9.rs
@@ -5,12 +5,7 @@ use num_bigint::BigInt;
 
 use crate::parser;
 
-#[derive(Debug, Clone, Copy)]
-struct DiskAlloc {
-    id: u32,
-    size: u32,
-    free_space: u32,
-}
+const EMPTY: i32 = -1;
 
 pub fn part1() -> u32 {
     let lines: Vec<String> = parser::read("data/day9.txt").unwrap();
@@ -36,85 +31,64 @@ fn parse_disk_map(lines: Vec<String>) -> Vec<u32> {
 }
 
 fn compact_checksum(disk_map: &mut Vec<u32>) -> BigInt {
-    let mut disk_alloc = VecDeque::from(into_disk_alloc(disk_map));
-    let mut compact_alloc = Vec::new();
-
-    let mut front_node = disk_alloc.pop_front();
-    let mut back_node = disk_alloc.pop_back();
+    let mut disk_alloc = into_disk_alloc(disk_map);
+    let mut front_idx = 0;
+    let mut back_idx = disk_alloc.len() - 1;
 
     loop {
-        while front_node.is_some_and(|node| node.free_space == 0) {
-            compact_alloc.push(front_node.unwrap());
-            front_node = disk_alloc.pop_front();
+        while disk_alloc[front_idx] != EMPTY {
+            front_idx += 1;
         }
-        while back_node.is_some_and(|node| node.size == 0) {
-            back_node = disk_alloc.pop_back();
+        while disk_alloc[back_idx] == EMPTY {
+            back_idx -= 1;
         }
-        if front_node.is_none() || back_node.is_none() {
+        if front_idx >= back_idx {
             break;
         }
 
-        let mut free_space_node = front_node.unwrap();
-        let mut to_compact_node = back_node.unwrap();
-
-        let space_after_compact = free_space_node.free_space as i32 - to_compact_node.size as i32;
-
-        let compacted_node = DiskAlloc {
-            id: to_compact_node.id,
-            size: to_compact_node.size.min(free_space_node.free_space),
-            free_space: space_after_compact.max(0) as u32,
-        };
-
-        free_space_node.free_space = 0;
-        compact_alloc.push(free_space_node);
-
-        to_compact_node.size = space_after_compact.neg().max(0) as u32;
-
-        front_node = Some(compacted_node);
-        back_node = Some(to_compact_node);
+        disk_alloc.swap(front_idx, back_idx);
     }
-
-    if let Some(node) = front_node {
-        compact_alloc.push(node);
-    }
-    if let Some(node) = back_node {
-        compact_alloc.push(node);
-    }
-
-    checksum(&compact_alloc)
-}
-
-fn whole_file_compact_checksum(disk_map: &mut Vec<u32>) -> BigInt {
-    let disk_alloc = into_disk_alloc(disk_map);
 
     checksum(&disk_alloc)
 }
 
-fn into_disk_alloc(disk_map: &mut Vec<u32>) -> Vec<DiskAlloc> {
-    let mut disk_alloc = Vec::new();
-    disk_map.push(0);
+fn whole_file_compact_checksum(disk_map: &mut Vec<u32>) -> BigInt {
+    // let mut disk_alloc = VecDeque::from(into_disk_alloc(disk_map));
+    // let mut compact_alloc = Vec::new();
 
+    // let mut front_node = disk_alloc.pop_front();
+    // let mut back_node = disk_alloc.pop_back();
+
+    // checksum(&disk_alloc)
+    0.into()
+}
+
+fn into_disk_alloc(disk_map: &mut Vec<u32>) -> Vec<i32> {
+    let size: u32 = disk_map.iter().sum();
+    let mut disk_alloc = vec![EMPTY; size as usize];
+    let mut alloc_id = 0;
+
+    disk_map.push(0);
     for (idx, (file_size, free_space_size)) in disk_map.iter().tuples().enumerate() {
-        disk_alloc.push(DiskAlloc {
-            id: idx as u32,
-            size: *file_size,
-            free_space: *free_space_size,
+        let id = idx as i32;
+
+        (0..*file_size).for_each(|_| {
+            disk_alloc[alloc_id] = id;
+            alloc_id += 1;
         });
+        alloc_id += *free_space_size as usize;
     }
 
     disk_alloc
 }
 
-fn checksum(disk_alloc: &Vec<DiskAlloc>) -> BigInt {
+fn checksum(disk_alloc: &Vec<i32>) -> BigInt {
     let mut checksum: BigInt = 0.into();
-    let mut checksum_idx = 0;
 
-    for disk in disk_alloc {
-        (0..disk.size).for_each(|_| {
-            checksum += disk.id * checksum_idx;
-            checksum_idx += 1;
-        });
-        checksum_idx += disk.free_space;
+    for (idx, id) in disk_alloc.iter().enumerate() {
+        if *id >= 0 {
+            checksum += *id as u32 * idx as u32
+        }
     }
 
     checksum
