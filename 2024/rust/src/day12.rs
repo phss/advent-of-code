1577e3fa83b230f647729000654480731671fda2
Paulo Schneider <paulo.schneider@gmail.com>
2024-12-31 10:57:55 +0000

    Refactor solution 12 to have a calculate_perimeter function

--- 2024/rust/src/day12.rs
+++ 2024/rust/src/day12.rs
@@ -19,9 +19,12 @@ fn total_price(map: &Vec<String>) -> u32 {
         for (x, region) in row.chars().enumerate() {
             let position = (x, y);
             if !visited.contains(&position) {
-                let (area, perimeter, region_nodes) = price_for_region(map, region, position);
+                let region_nodes = get_region_nodes(map, region, position);
 
+                let area = region_nodes.len() as u32;
+                let perimeter = calculate_perimeter(map, region, &region_nodes);
                 price += area * perimeter;
+
                 visited.extend(region_nodes);
             }
         }
@@ -30,13 +33,16 @@ fn total_price(map: &Vec<String>) -> u32 {
     price
 }
 
-fn price_for_region(
+fn total_price_with_discount(map: &Vec<String>) -> u32 {
+    0
+}
+
+fn get_region_nodes(
     map: &Vec<String>,
     region: char,
     start: (usize, usize),
-) -> (u32, u32, HashSet<(usize, usize)>) {
+) -> HashSet<(usize, usize)> {
     let mut region_nodes = HashSet::new();
-    let mut perimeter = 0;
     let mut to_visit = vec![start];
 
     while let Some(position @ (x, y)) = to_visit.pop() {
@@ -57,13 +63,39 @@ fn price_for_region(
 
             if new_region == Some(region) {
                 to_visit.push(new_position.unwrap());
-            } else {
-                perimeter += 1;
             }
         }
     }
 
-    (region_nodes.len() as u32, perimeter, region_nodes)
+    region_nodes
+}
+
+fn calculate_perimeter(
+    map: &Vec<String>,
+    region: char,
+    region_nodes: &HashSet<(usize, usize)>,
+) -> u32 {
+    let directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    region_nodes
+        .iter()
+        .flat_map(|(x, y)| {
+            directions.iter().map(|(dir_x, dir_y)| {
+                let new_x = x.checked_add_signed(*dir_x);
+                let new_y = y.checked_add_signed(*dir_y);
+                let new_position = new_x.zip(new_y);
+
+                let new_region = new_position.and_then(|(new_x, new_y)| {
+                    map.get(new_y).unwrap_or(&String::new()).chars().nth(new_x)
+                });
+
+                if new_region != Some(region) {
+                    1
+                } else {
+                    0
+                }
+            })
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -92,5 +124,23 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let map = vec![
+            "RRRRIICCFF",
+            "RRRRIICCCF",
+            "VVRRRCCFFF",
+            "VVRCCCJFFF",
+            "VVVVCJJCFE",
+            "VVIVCCJJEE",
+            "VVIIICJJEE",
+            "MIIIIIJJEE",
+            "MIIISIJEEE",
+            "MMMISSJEEE",
+        ];
+        let map: Vec<String> = map.into_iter().map(|s| s.to_string()).collect();
+
+        let result = total_price_with_discount(&map);
+
+        // assert_eq!(result, 1206)
+    }
 }
