5763cbe2a4ef954b9512fb9e7a9ecea99725ec04
Paulo Schneider <paulo.schneider@gmail.com>
2024-12-31 11:41:03 +0000

    Hacky solution for day 12 part 2

--- 2024/rust/src/day12.rs
+++ 2024/rust/src/day12.rs
@@ -1,4 +1,6 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+
+use itertools::Itertools;
 
 use crate::parser;
 
@@ -8,7 +10,8 @@ pub fn part1() -> u32 {
 }
 
 pub fn part2() -> u32 {
-    0
+    let map: Vec<String> = parser::read("data/day12.txt").unwrap();
+    total_price_with_discount(&map)
 }
 
 fn total_price(map: &Vec<String>) -> u32 {
@@ -34,7 +37,25 @@ fn total_price(map: &Vec<String>) -> u32 {
 }
 
 fn total_price_with_discount(map: &Vec<String>) -> u32 {
-    0
+    let mut price = 0;
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+
+    for (y, row) in map.iter().enumerate() {
+        for (x, region) in row.chars().enumerate() {
+            let position = (x, y);
+            if !visited.contains(&position) {
+                let region_nodes = get_region_nodes(map, region, position);
+
+                let area = region_nodes.len() as u32;
+                let sides = calculate_sides(map, region, &region_nodes);
+                price += area * sides;
+
+                visited.extend(region_nodes);
+            }
+        }
+    }
+
+    price
 }
 
 fn get_region_nodes(
@@ -98,6 +119,76 @@ fn calculate_perimeter(
         .sum()
 }
 
+fn calculate_sides(map: &Vec<String>, region: char, region_nodes: &HashSet<(usize, usize)>) -> u32 {
+    let horizontal_directions = [(1, 0), (-1, 0)];
+    let vertical_directions = [(0, 1), (0, -1)];
+    let directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+    let mut sides_by_direction = HashMap::new();
+    for (x, y) in region_nodes {
+        for direction @ (dir_x, dir_y) in directions {
+            let new_x = *x as isize + dir_x;
+            let new_y = *y as isize + dir_y;
+
+            let new_region = if new_x >= 0 && new_y >= 0 {
+                map.get(new_y as usize)
+                    .unwrap_or(&String::new())
+                    .chars()
+                    .nth(new_x as usize)
+            } else {
+                None
+            };
+
+            if new_region != Some(region) {
+                sides_by_direction
+                    .entry(direction)
+                    .or_insert(Vec::new())
+                    .push((new_x, new_y));
+            }
+        }
+    }
+
+    let mut sides = 0;
+
+    for direction in horizontal_directions {
+        let blah = sides_by_direction
+            .get(&direction)
+            .unwrap()
+            .iter()
+            .into_group_map_by(|(x, _)| x)
+            .into_values();
+        for b in blah {
+            sides += 1 + b
+                .iter()
+                .map(|(_, y)| y)
+                .sorted()
+                .tuple_windows()
+                .filter(|(a, b)| (*b - *a) > 1)
+                .count();
+        }
+    }
+
+    for direction in vertical_directions {
+        let blah = sides_by_direction
+            .get(&direction)
+            .unwrap()
+            .iter()
+            .into_group_map_by(|(_, y)| y)
+            .into_values();
+        for b in blah {
+            sides += 1 + b
+                .iter()
+                .map(|(x, _)| x)
+                .sorted()
+                .tuple_windows()
+                .filter(|(a, b)| (*b - *a) > 1)
+                .count();
+        }
+    }
+
+    sides as u32
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -141,6 +232,6 @@ mod tests {
 
         let result = total_price_with_discount(&map);
 
-        // assert_eq!(result, 1206)
+        assert_eq!(result, 1206)
     }
 }
