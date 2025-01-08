54a4dbbff09eda19367e41b2d4923292044015f9
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-08 11:59:15 +0000

    Solution for day 19 part 2

--- 2024/rust/src/day19.rs
+++ 2024/rust/src/day19.rs
@@ -5,7 +5,7 @@ use crate::parser;
 struct Solver {
     patterns: HashSet<String>,
     max_pattern_size: usize,
-    cache: HashMap<usize, bool>,
+    cache: HashMap<usize, usize>,
 }
 
 impl Solver {
@@ -19,19 +19,24 @@ impl Solver {
     }
 
     fn is_valid_design(&mut self, start: usize, towel: &String) -> bool {
+        self.design_permutations(start, towel) > 0
+    }
+
+    fn design_permutations(&mut self, start: usize, towel: &String) -> usize {
         if start >= towel.len() {
-            return true;
+            return 1;
         } else if self.cache.contains_key(&start) {
             return *self.cache.get(&start).unwrap();
         }
 
-        let result = (1..=self.max_pattern_size).rev().any(|size| {
+        let mut result = 0;
+        (1..=self.max_pattern_size).rev().for_each(|size| {
             let end = start + size;
             if end <= towel.len() {
                 let sub_towel = &towel[start..end];
-                self.patterns.contains(sub_towel) && self.is_valid_design(end, towel)
-            } else {
-                false
+                if self.patterns.contains(sub_towel) {
+                    result += self.design_permutations(end, towel);
+                }
             }
         });
         self.cache.insert(start, result);
@@ -47,6 +52,10 @@ pub fn part1() -> u32 {
 }
 
 pub fn part2() -> u32 {
+    let lines: Vec<String> = parser::read("data/day19.txt").unwrap();
+    let (patterns, towels) = parse(&lines);
+    let result = all_possible_designs_ways_2000(&patterns, &towels);
+    println!("Result {result}");
     0
 }
 
@@ -55,6 +64,13 @@ fn possible_designs(patterns: &HashSet<String>, towels: &Vec<String>) -> usize {
         .iter()
         .filter(|towel| Solver::new(patterns).is_valid_design(0, towel))
         .count()
+}
+
+fn all_possible_designs_ways_2000(patterns: &HashSet<String>, towels: &Vec<String>) -> usize {
+    towels
+        .iter()
+        .map(|towel| Solver::new(patterns).design_permutations(0, towel))
+        .sum()
 }
 
 fn parse(lines: &Vec<String>) -> (HashSet<String>, Vec<String>) {
@@ -104,5 +120,24 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let lines = vec![
+            "r, wr, b, g, bwu, rb, gb, br",
+            "",
+            "brwrr",
+            "bggr",
+            "gbbr",
+            "rrbgbr",
+            "ubwu",
+            "bwurrg",
+            "brgr",
+            "bbrgwb",
+        ];
+        let lines: Vec<String> = lines.into_iter().map(|s| s.to_string()).collect();
+        let (patterns, towels) = parse(&lines);
+
+        let result = all_possible_designs_ways_2000(&patterns, &towels);
+
+        assert_eq!(result, 16)
+    }
 }
