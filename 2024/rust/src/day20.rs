a810d51a5133ae5097f5f595b1ef4dbc337a006f
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-08 15:23:08 +0000

    Solution for day 20 part 2

--- 2024/rust/src/day20.rs
+++ 2024/rust/src/day20.rs
@@ -1,34 +1,41 @@
 mod map;
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet};
 
 use crate::parser;
 
 pub fn part1() -> u32 {
     let lines: Vec<String> = parser::read("data/day20.txt").unwrap();
     let map = map::parse(&lines);
-    count_cheats(&map, 100) as u32
+    count_cheats(&map, 2, 100) as u32
 }
 
 pub fn part2() -> u32 {
-    0
+    let lines: Vec<String> = parser::read("data/day20.txt").unwrap();
+    let map = map::parse(&lines);
+    count_cheats(&map, 20, 100) as u32
 }
 
-fn count_cheats(map: &Vec<Vec<char>>, time_saved: u32) -> u32 {
+fn count_cheats(map: &Vec<Vec<char>>, cheat_steps: usize, time_saved: usize) -> u32 {
     let mut count = 0;
-    let start = map::position(map, 'S');
-    let end = map::position(map, 'E');
-    let shortest_path_without_cheats = shortest_path(map, start, end);
+    let shortest_path = shortest_path(map, map::position(map, 'S'), map::position(map, 'E'));
+    let shortest_path_steps = shortest_path.len() - 1;
 
-    for y in 1..map.len() - 1 {
-        for x in 1..map[y].len() - 1 {
-            if map[y][x] == '#' {
-                let mut cheated_map = map.clone();
-                cheated_map[y][x] = 'x';
-                let cheated_shortest_path = shortest_path(&cheated_map, start, end);
-                if (shortest_path_without_cheats - cheated_shortest_path) >= time_saved {
+    let mut steps_from_start = HashMap::new();
+    let mut steps_to_end = HashMap::new();
+
+    for (steps, position) in shortest_path.iter().enumerate() {
+        steps_from_start.insert(position, steps);
+        steps_to_end.insert(position, shortest_path_steps - steps);
+    }
+
+    for (a, start_to_a) in &steps_from_start {
+        for (b, b_to_end) in &steps_to_end {
+            let a_to_b = distance(a, b);
+            if a_to_b <= cheat_steps {
+                let cheat_distance = *start_to_a + *b_to_end + a_to_b;
+                if shortest_path_steps as isize - cheat_distance as isize >= time_saved as isize {
                     count += 1;
                 }
-                println!("{:?} -> {}", (x, y), count);
             }
         }
     }
@@ -36,15 +43,19 @@ fn count_cheats(map: &Vec<Vec<char>>, time_saved: u32) -> u32 {
     count
 }
 
-fn shortest_path(map: &Vec<Vec<char>>, start: (usize, usize), end: (usize, usize)) -> u32 {
+fn shortest_path(
+    map: &Vec<Vec<char>>,
+    start: (usize, usize),
+    end: (usize, usize),
+) -> Vec<(usize, usize)> {
     let directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
     let mut visited = HashSet::new();
-    let mut search_queue = VecDeque::new();
-    search_queue.push_back((start, 0));
-    while let Some((position, steps)) = search_queue.pop_front() {
-        if position == end {
-            return steps;
-        }
+    let mut path = Vec::new();
+
+    let mut position = start;
+    while position != end {
+        path.push(position);
+        visited.insert(position);
 
         let (x, y) = position;
         for (dir_x, dir_y) in directions {
@@ -56,12 +67,19 @@ fn shortest_path(map: &Vec<Vec<char>>, start: (usize, usize), end: (usize, usize
                 continue;
             }
 
-            search_queue.push_back((next_position, steps + 1));
-            visited.insert(next_position);
+            position = next_position;
+            break;
         }
     }
+    path.push(end);
 
-    0
+    path
+}
+
+fn distance(a: &(usize, usize), b: &(usize, usize)) -> usize {
+    let (x1, y1) = *a;
+    let (x2, y2) = *b;
+    ((x1 as isize - x2 as isize).abs() + (y1 as isize - y2 as isize).abs()) as usize
 }
 
 #[cfg(test)]
@@ -90,7 +108,8 @@ mod tests {
         let lines: Vec<String> = lines.into_iter().map(|s| s.to_string()).collect();
         let map = map::parse(&lines);
 
-        assert_eq!(count_cheats(&map, 20), 5);
+        assert_eq!(count_cheats(&map, 2, 20), 5);
+        assert_eq!(count_cheats(&map, 2, 12), 8);
     }
 
     #[test]
