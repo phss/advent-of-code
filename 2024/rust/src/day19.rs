533e71f917e80d1147e2537bfc10199a171d81cc
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-08 09:27:44 +0000

    Setup for day 19

---
+++ 2024/rust/src/day19.rs
@@ -0,0 +1,25 @@
+use crate::parser;
+
+
+pub fn part1() -> u32 {
+    0
+}
+
+pub fn part2() -> u32 {
+    0
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_input_part_1() {
+    }
+
+    #[test]
+    fn sample_input_part_2() {
+    }
+
+}
