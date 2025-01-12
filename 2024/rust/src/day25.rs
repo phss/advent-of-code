1ff511fcfcbb6918a416e982b89f8cf261d08127
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-12 09:18:31 +0000

    Solution for day 25 part 1

--- 2024/rust/src/day25.rs
+++ 2024/rust/src/day25.rs
@@ -1,11 +1,54 @@
 use crate::parser;
 
 pub fn part1() -> u32 {
-    0
+    let lines: Vec<String> = parser::read("data/day25.txt").unwrap();
+    let (locks, keys) = parse(&lines);
+    count_fits(&locks, &keys) as u32
 }
 
 pub fn part2() -> u32 {
     0
+}
+
+fn parse(lines: &Vec<String>) -> (Vec<Vec<usize>>, Vec<Vec<usize>>) {
+    let mut locks = Vec::new();
+    let mut keys = Vec::new();
+    let parts = lines.split(|line| line.is_empty());
+
+    for part in parts {
+        let mut heights = Vec::new();
+        for col in 0..5 {
+            let mut height = 0;
+            for row in part {
+                if row.chars().nth(col) == Some('#') {
+                    height += 1;
+                }
+            }
+            heights.push(height - 1);
+        }
+
+        if part[0] == "#####" {
+            locks.push(heights);
+        } else {
+            keys.push(heights);
+        }
+    }
+
+    (locks, keys)
+}
+
+fn count_fits(locks: &Vec<Vec<usize>>, keys: &Vec<Vec<usize>>) -> usize {
+    let mut count = 0;
+
+    for lock in locks {
+        for key in keys {
+            if lock.iter().zip(key.iter()).all(|(l, k)| (l + k) <= 5) {
+                count += 1;
+            }
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -13,7 +56,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sample_input_part_1() {}
+    fn sample_input_part_1() {
+        let lines = vec![
+            "#####", ".####", ".####", ".####", ".#.#.", ".#...", ".....", "", "#####", "##.##",
+            ".#.##", "...##", "...#.", "...#.", ".....", "", ".....", "#....", "#....", "#...#",
+            "#.#.#", "#.###", "#####", "", ".....", ".....", "#.#..", "###..", "###.#", "###.#",
+            "#####", "", ".....", ".....", ".....", "#....", "#.#..", "#.#.#", "#####",
+        ];
+        let lines: Vec<String> = lines.into_iter().map(|s| s.to_string()).collect();
+        let (locks, keys) = parse(&lines);
+
+        let result = count_fits(&locks, &keys);
+
+        assert_eq!(result, 3)
+    }
 
     #[test]
     fn sample_input_part_2() {}
