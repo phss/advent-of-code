725d323c72d0d98f359de0f0fbc2dfa1b62e38d7
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-13 08:10:41 +0000

    Solution for day 1 part 1

--- 2023/rust/src/day1.rs
+++ 2023/rust/src/day1.rs
@@ -1,14 +1,27 @@
+use core::num;
+
 use crate::parser;
 
-
 pub fn part1() -> u32 {
-    0
+    let doc: Vec<String> = parser::read("data/day1.txt").unwrap();
+    calibration_values(&doc) as u32
 }
 
 pub fn part2() -> u32 {
     0
 }
 
+fn calibration_values(doc: &Vec<String>) -> usize {
+    doc.iter()
+        .map(|line| {
+            let number_chars: Vec<char> = line.chars().filter(|c| c.is_ascii_digit()).collect();
+            let mut number_str = String::new();
+            number_str.push(*number_chars.first().unwrap());
+            number_str.push(*number_chars.last().unwrap());
+            number_str.parse::<usize>().unwrap()
+        })
+        .sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -16,10 +29,14 @@ mod tests {
 
     #[test]
     fn sample_input_part_1() {
+        let doc = vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+        let doc: Vec<String> = doc.into_iter().map(|s| s.to_string()).collect();
+
+        let result = calibration_values(&doc);
+
+        assert_eq!(result, 142)
     }
 
     #[test]
-    fn sample_input_part_2() {
-    }
-
+    fn sample_input_part_2() {}
 }
