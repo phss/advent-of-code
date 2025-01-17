8c26ea84696a6666e71aeb59c2396b4ed8c0f571
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-17 10:01:42 +0000

    Solution for day 6 part 1

--- 2023/rust/src/day6.rs
+++ 2023/rust/src/day6.rs
@@ -1,11 +1,36 @@
-use crate::parser;
-
 pub fn part1() -> usize {
-    0
+    let times = vec![34, 90, 89, 86];
+    let distances = vec![204, 1713, 1210, 1780];
+    mult_of_number_of_ways(&times, &distances)
 }
 
 pub fn part2() -> usize {
     0
+}
+
+fn mult_of_number_of_ways(times: &Vec<usize>, distances: &Vec<usize>) -> usize {
+    let mut mult = 1;
+
+    times
+        .iter()
+        .zip(distances.iter())
+        .for_each(|(time, distance_goal)| mult *= num_of_ways_to_win(*time, *distance_goal));
+
+    mult
+}
+
+fn num_of_ways_to_win(time: usize, distance_goal: usize) -> usize {
+    let mut ways = 0;
+
+    for pressed in 1..=time {
+        let remaining_time = time - pressed;
+        let distance = remaining_time * pressed;
+        if distance > distance_goal {
+            ways += 1;
+        }
+    }
+
+    ways
 }
 
 #[cfg(test)]
@@ -13,7 +38,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sample_input_part_1() {}
+    fn sample_input_part_1() {
+        let times = vec![7, 15, 30];
+        let distances = vec![9, 40, 200];
+
+        let result = mult_of_number_of_ways(&times, &distances);
+
+        assert_eq!(result, 288);
+    }
 
     #[test]
     fn sample_input_part_2() {}
