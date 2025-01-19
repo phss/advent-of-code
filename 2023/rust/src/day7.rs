ee1abf6b5d2bbf4c9dc4b06950add101b7c0166b
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-19 10:41:19 +0000

    Solution for day 7 part 2

--- 2023/rust/src/day7.rs
+++ 2023/rust/src/day7.rs
@@ -8,13 +8,31 @@ use crate::parser;
 struct Hand {
     cards: String,
     bid: usize,
+    jokered: bool,
 }
 
 impl Hand {
     fn hand_type_strength(&self) -> usize {
         let card_groups = self.cards.chars().into_group_map_by(|it| *it);
-        let different_cards = card_groups.len();
-        let biggest_card_group = card_groups.values().map(|g| g.len()).max().unwrap();
+        let mut different_cards = card_groups.len();
+        let mut biggest_card_group = card_groups.values().map(|g| g.len()).max().unwrap();
+
+        if self.jokered {
+            biggest_card_group = card_groups
+                .iter()
+                .filter(|(c, _)| **c != 'J')
+                .map(|(_, v)| v)
+                .map(|g| g.len())
+                .max()
+                .unwrap_or(0);
+            let jokers = card_groups.get(&'J').map(|g| g.len()).unwrap_or(0);
+            if jokers > 0 {
+                biggest_card_group += jokers;
+                different_cards -= 1;
+                different_cards = different_cards.max(1);
+            }
+        }
+
         match (different_cards, biggest_card_group) {
             (1, _) => 7,
             (2, 4) => 6,
@@ -23,7 +41,7 @@ impl Hand {
             (3, 2) => 3,
             (4, _) => 2,
             (5, _) => 1,
-            _ => panic!("should be unreachable"),
+            e => panic!("should be unreachable: {:?}, {}", e, self.cards),
         }
     }
 
@@ -34,6 +52,7 @@ impl Hand {
                 'A' => 14,
                 'K' => 13,
                 'Q' => 12,
+                'J' if self.jokered => 1,
                 'J' => 11,
                 'T' => 10,
                 c => c.to_string().parse().unwrap(),
@@ -50,7 +69,11 @@ impl FromStr for Hand {
         let cards = parts.next().unwrap().to_string();
         let bid = parts.next().unwrap().parse().unwrap();
 
-        Ok(Hand { cards, bid })
+        Ok(Hand {
+            cards,
+            bid,
+            jokered: false,
+        })
     }
 }
 
@@ -74,12 +97,27 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-    0
+    let hands: Vec<Hand> = parser::read("data/day7.txt").unwrap();
+    total_winnings_with_joker(&hands)
 }
 
 fn total_winnings(hands: &Vec<Hand>) -> usize {
     hands
         .iter()
+        .sorted()
+        .enumerate()
+        .map(|(i, hand)| (i + 1) * hand.bid)
+        .sum()
+}
+
+fn total_winnings_with_joker(hands: &Vec<Hand>) -> usize {
+    hands
+        .iter()
+        .map(|hand| Hand {
+            cards: hand.cards.clone(),
+            bid: hand.bid,
+            jokered: true,
+        })
         .sorted()
         .enumerate()
         .map(|(i, hand)| (i + 1) * hand.bid)
@@ -107,5 +145,18 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let lines = vec![
+            "32T3K 765",
+            "T55J5 684",
+            "KK677 28",
+            "KTJJT 220",
+            "QQQJA 483",
+        ];
+        let hands: Vec<Hand> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
+
+        let result = total_winnings_with_joker(&hands);
+
+        assert_eq!(result, 5905)
+    }
 }
