d7190c70e84ca96ee60bb2c38018f330727622d3
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-20 09:56:37 +0000

    Slow solution for day 8 part 1

--- 2023/rust/src/day8.rs
+++ 2023/rust/src/day8.rs
@@ -9,7 +9,9 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-    0
+    let lines: Vec<String> = parser::read("data/day8.txt").unwrap();
+    let (moves, network) = parse(&lines);
+    step_count_multiple(&moves, &network)
 }
 
 fn parse(lines: &Vec<String>) -> (Vec<char>, HashMap<String, (String, String)>) {
@@ -42,6 +44,34 @@ fn step_count(moves: &Vec<char>, network: &HashMap<String, (String, String)>) ->
         } else {
             right.clone()
         };
+
+        i += 1;
+        i = i % moves.len();
+        steps += 1;
+    }
+
+    steps
+}
+
+fn step_count_multiple(moves: &Vec<char>, network: &HashMap<String, (String, String)>) -> usize {
+    let mut nodes: Vec<String> = network
+        .keys()
+        .filter(|node| node.ends_with("A"))
+        .cloned()
+        .collect();
+    let mut i = 0;
+    let mut steps = 0;
+
+    while nodes.iter().any(|node| !node.ends_with("Z")) {
+        nodes.iter_mut().for_each(|node| {
+            let (left, right) = network.get(node).unwrap();
+
+            *node = if moves[i] == 'L' {
+                left.clone()
+            } else {
+                right.clone()
+            };
+        });
 
         i += 1;
         i = i % moves.len();
@@ -94,5 +124,24 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let lines = vec![
+            "LR",
+            "",
+            "11A = (11B, XXX)",
+            "11B = (XXX, 11Z)",
+            "11Z = (11B, XXX)",
+            "22A = (22B, XXX)",
+            "22B = (22C, 22C)",
+            "22C = (22Z, 22Z)",
+            "22Z = (22B, 22B)",
+            "XXX = (XXX, XXX)",
+        ];
+        let lines: Vec<String> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
+        let (moves, network) = parse(&lines);
+
+        let result = step_count_multiple(&moves, &network);
+
+        assert_eq!(result, 6);
+    }
 }
