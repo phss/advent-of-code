719a49e28ac663a92a664db9b25b88d40789fca0
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-25 09:59:12 +0000

    Solution for day 12 part 1

--- 2023/rust/src/day12.rs
+++ 2023/rust/src/day12.rs
@@ -29,11 +29,52 @@ impl FromStr for Record {
 }
 
 pub fn part1() -> usize {
-    0
+    let records: Vec<Record> = parser::read("data/day12.txt").unwrap();
+    sum_of_arrangements(&records)
 }
 
 pub fn part2() -> usize {
     0
+}
+
+fn sum_of_arrangements(records: &Vec<Record>) -> usize {
+    records
+        .iter()
+        .map(|record| arrangements(&record.field, record.damaged.clone()))
+        .sum()
+}
+
+fn arrangements(field: &str, damaged: Vec<usize>) -> usize {
+    if field.len() == 0 {
+        return if damaged.len() == 0 { 1 } else { 0 };
+    }
+
+    let mut count = 0;
+
+    let head = field.chars().nth(0).unwrap();
+    if head == '.' || head == '?' {
+        count += arrangements(&field[1..], damaged.clone());
+    }
+
+    if head == '#' || head == '?' {
+        if let Some(damage) = damaged.first() {
+            let can_fit_damage =
+                *damage <= field.len() && field[0..*damage].chars().all(|c| c == '#' || c == '?');
+            let followed_by_space =
+                *damage == field.len() || field.chars().nth(*damage) != Some('#');
+
+            if can_fit_damage && followed_by_space {
+                let damage_size = if *damage == field.len() {
+                    *damage
+                } else {
+                    *damage + 1
+                };
+                count += arrangements(&field[damage_size..], damaged[1..].to_vec());
+            }
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -52,7 +93,9 @@ mod tests {
         ];
         let records: Vec<Record> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
 
-        println!("{:?}", records);
+        let result = sum_of_arrangements(&records);
+
+        assert_eq!(result, 21);
     }
 
     #[test]
