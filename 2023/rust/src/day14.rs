da1c32b4f87e9b27e404a2342c54a9046ef39285
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-28 08:44:13 +0000

    Solution for day 14 part 1

--- 2023/rust/src/day14.rs
+++ 2023/rust/src/day14.rs
@@ -1,11 +1,40 @@
 use crate::parser;
 
 pub fn part1() -> usize {
-    0
+    let lines: Vec<String> = parser::read("data/day14.txt").unwrap();
+    let map = parse(&lines);
+    calculate_load(&map)
 }
 
 pub fn part2() -> usize {
     0
+}
+
+fn calculate_load(map: &Vec<Vec<char>>) -> usize {
+    let width = map[0].len();
+    let height = map.len();
+    let mut load = 0;
+
+    for x in 0..width {
+        let mut col_load = height;
+
+        for y in 0..height {
+            match map[y][x] {
+                'O' => {
+                    load += col_load;
+                    col_load -= 1;
+                }
+                '#' => col_load = height - y - 1,
+                _ => {}
+            }
+        }
+    }
+
+    load
+}
+
+pub fn parse(lines: &Vec<String>) -> Vec<Vec<char>> {
+    lines.iter().map(|s| s.chars().collect()).collect()
 }
 
 #[cfg(test)]
@@ -13,7 +42,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sample_input_part_1() {}
+    fn sample_input_part_1() {
+        let lines = vec![
+            "O....#....",
+            "O.OO#....#",
+            ".....##...",
+            "OO.#O....O",
+            ".O.....O#.",
+            "O.#..O.#.#",
+            "..O..#O..O",
+            ".......O..",
+            "#....###..",
+            "#OO..#....",
+        ];
+        let lines: Vec<String> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
+        let map = parse(&lines);
+
+        let result = calculate_load(&map);
+
+        assert_eq!(result, 136);
+    }
 
     #[test]
     fn sample_input_part_2() {}
