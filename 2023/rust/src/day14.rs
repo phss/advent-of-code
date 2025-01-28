25ca0043704cb0ab9923c49ad05f06dba391c9b9
Paulo Schneider <paulo.schneider@gmail.com>
2025-01-28 09:00:11 +0000

    Refactor to actually tilt

--- 2023/rust/src/day14.rs
+++ 2023/rust/src/day14.rs
@@ -2,12 +2,33 @@ use crate::parser;
 
 pub fn part1() -> usize {
     let lines: Vec<String> = parser::read("data/day14.txt").unwrap();
-    let map = parse(&lines);
+    let mut map = parse(&lines);
+    tilt_north(&mut map);
     calculate_load(&map)
 }
 
 pub fn part2() -> usize {
     0
+}
+
+fn tilt_north(map: &mut Vec<Vec<char>>) {
+    let width = map[0].len();
+    let height = map.len();
+
+    for x in 0..width {
+        let mut free_space_y = 0;
+
+        for y in 0..height {
+            match map[y][x] {
+                'O' => {
+                    (map[free_space_y][x], map[y][x]) = (map[y][x], map[free_space_y][x]);
+                    free_space_y += 1;
+                }
+                '#' => free_space_y = y + 1,
+                _ => {}
+            }
+        }
+    }
 }
 
 fn calculate_load(map: &Vec<Vec<char>>) -> usize {
@@ -16,15 +37,11 @@ fn calculate_load(map: &Vec<Vec<char>>) -> usize {
     let mut load = 0;
 
     for x in 0..width {
-        let mut col_load = height;
-
         for y in 0..height {
             match map[y][x] {
                 'O' => {
-                    load += col_load;
-                    col_load -= 1;
+                    load += height - y;
                 }
-                '#' => col_load = height - y - 1,
                 _ => {}
             }
         }
@@ -56,8 +73,9 @@ mod tests {
             "#OO..#....",
         ];
         let lines: Vec<String> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
-        let map = parse(&lines);
+        let mut map = parse(&lines);
 
+        tilt_north(&mut map);
         let result = calculate_load(&map);
 
         assert_eq!(result, 136);
