30aaacb3f70873819319a810c964e745e057d58a
Paulo Schneider <paulo.schneider@gmail.com>
2025-02-03 08:36:18 +0000

    Solution for day 16 part 2

--- 2023/rust/src/day17.rs
+++ 2023/rust/src/day17.rs
@@ -34,7 +34,9 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-    0
+    let lines: Vec<String> = parser::read("data/day17.txt").unwrap();
+    let map = parse(&lines);
+    min_heat_loss_ultra(&map)
 }
 
 fn min_heat_loss(map: &Vec<Vec<usize>>) -> usize {
@@ -99,6 +101,69 @@ fn min_heat_loss(map: &Vec<Vec<usize>>) -> usize {
     lowest_cost
 }
 
+fn min_heat_loss_ultra(map: &Vec<Vec<usize>>) -> usize {
+    let width = map[0].len();
+    let height = map.len();
+
+    let start_position = (0, 0);
+    let end_position = (width - 1, height - 1);
+
+    let mut visited = HashSet::new();
+    let mut search_heap = BinaryHeap::new();
+    search_heap.push(Path {
+        cost: 0,
+        position: start_position,
+        direction: (0, 0),
+        straight: 0,
+        visited: HashSet::new(),
+    });
+
+    let mut lowest_cost = usize::MAX;
+    let directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    while let Some(path) = search_heap.pop() {
+        if path.position == end_position && path.straight >= 4 {
+            lowest_cost = lowest_cost.min(path.cost);
+            break;
+        }
+
+        let (x, y) = path.position;
+        for direction @ (dir_x, dir_y) in directions {
+            let next_x = x.checked_add_signed(dir_x).unwrap_or(0).min(width - 1);
+            let next_y = y.checked_add_signed(dir_y).unwrap_or(0).min(height - 1);
+            let next_position = (next_x, next_y);
+            let straight = if direction == path.direction {
+                path.straight + 1
+            } else {
+                1
+            };
+
+            if next_position == path.position
+                || visited.contains(&(next_position, direction, straight))
+                || path.visited.contains(&next_position)
+                || straight > 10
+                || (path.direction != direction && path.straight < 4 && path.direction != (0, 0))
+            {
+                continue;
+            }
+
+            let move_cost = map[next_y][next_x];
+            let mut path_visited = path.visited.clone();
+            path_visited.insert(next_position);
+            search_heap.push(Path {
+                cost: path.cost + move_cost,
+                position: next_position,
+                direction,
+                straight,
+                visited: path_visited,
+            });
+
+            visited.insert((next_position, direction, straight));
+        }
+    }
+
+    lowest_cost
+}
+
 fn parse(lines: &Vec<String>) -> Vec<Vec<usize>> {
     lines
         .iter()
@@ -136,5 +201,44 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let lines = vec![
+            "2413432311323",
+            "3215453535623",
+            "3255245654254",
+            "3446585845452",
+            "4546657867536",
+            "1438598798454",
+            "4457876987766",
+            "3637877979653",
+            "4654967986887",
+            "4564679986453",
+            "1224686865563",
+            "2546548887735",
+            "4322674655533",
+        ];
+        let lines: Vec<String> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
+        let map = parse(&lines);
+
+        let result = min_heat_loss_ultra(&map);
+
+        assert_eq!(result, 94);
+    }
+
+    #[test]
+    fn sample_input_part_2_extra() {
+        let lines = vec![
+            "111111111111",
+            "999999999991",
+            "999999999991",
+            "999999999991",
+            "999999999991",
+        ];
+        let lines: Vec<String> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
+        let map = parse(&lines);
+
+        let result = min_heat_loss_ultra(&map);
+
+        assert_eq!(result, 71);
+    }
 }
