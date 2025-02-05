4f62f509499d8db155af740a1da02858d409f877
Paulo Schneider <paulo.schneider@gmail.com>
2025-02-05 07:41:21 +0000

    Setup for day 19

--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -1,8 +1,8 @@
 use std::env;
 
 use advent_of_code_2024::{
-    day1, day10, day11, day12, day13, day14, day15, day16, day17, day18, day2, day3, day4, day5,
-    day6, day7, day8, day9,
+    day1, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day2, day3, day4,
+    day5, day6, day7, day8, day9,
 };
 
 fn parse_command_line_args() -> (u32, u32) {
@@ -56,6 +56,8 @@ fn run(day: u32, part: u32) -> usize {
         (17, 2) => day17::part2,
         (18, 1) => day18::part1,
         (18, 2) => day18::part2,
+        (19, 1) => day19::part1,
+        (19, 2) => day19::part2,
         _ => panic!("No solution yet for day {} and part {}", day, part),
     };
     solution()
