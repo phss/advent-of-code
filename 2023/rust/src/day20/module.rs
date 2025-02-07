5bbef32c092d488a87829043d386a6c36628e830
Paulo Schneider <paulo.schneider@gmail.com>
2025-02-07 08:47:05 +0000

    Parse flip flop from string

--- 2023/rust/src/day20/module.rs
+++ 2023/rust/src/day20/module.rs
@@ -10,9 +10,29 @@ impl FromStr for Broadcaster {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let parts: Vec<&str> = s.split("->").collect();
-        let values = parts[1].split(',').map(|s| s.trim().to_string()).collect();
-        Ok(Broadcaster {
-            destinations: values,
+        let destinations = parts[1].split(',').map(|s| s.trim().to_string()).collect();
+        Ok(Broadcaster { destinations })
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct FlipFlop {
+    label: String,
+    on: bool,
+    destinations: Vec<String>,
+}
+
+impl FromStr for FlipFlop {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split(" -> ").collect();
+        let label = parts[0].strip_prefix("%").unwrap().to_string();
+        let destinations = parts[1].split(',').map(|s| s.trim().to_string()).collect();
+        Ok(FlipFlop {
+            label,
+            on: false,
+            destinations,
         })
     }
 }
@@ -32,6 +52,24 @@ mod tests {
                 broadcaster,
                 Broadcaster {
                     destinations: vec!["a".to_string(), "b".to_string(), "c".to_string()]
+                }
+            );
+        }
+    }
+
+    mod flip_flop {
+        use super::*;
+
+        #[test]
+        fn create_from_str() {
+            let flip_flop = FlipFlop::from_str("%a -> b, c").unwrap();
+
+            assert_eq!(
+                flip_flop,
+                FlipFlop {
+                    label: "a".to_string(),
+                    on: false,
+                    destinations: vec!["b".to_string(), "c".to_string()]
                 }
             );
         }
