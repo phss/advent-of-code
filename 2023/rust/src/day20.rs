f6925cf3fc1c00a9295fd2124adfea55280c2fed
Paulo Schneider <paulo.schneider@gmail.com>
2025-02-13 08:51:40 +0000

    Fast solution for day 20 part 2

--- 2023/rust/src/day20.rs
+++ 2023/rust/src/day20.rs
@@ -1,6 +1,8 @@
 mod module;
 mod simulation;
 
+use std::collections::HashSet;
+
 use crate::parser;
 
 pub fn part1() -> usize {
@@ -27,23 +29,45 @@ fn pulse_mults(sim: &mut simulation::Simulation) -> usize {
 
 fn presses_until_rx(sim: &mut simulation::Simulation) -> usize {
     let mut presses = 0;
+    let mut remaining_fan_in_nodes = HashSet::from([
+        "qz".to_string(),
+        "cq".to_string(),
+        "jx".to_string(),
+        "tt".to_string(),
+    ]);
+    let mut activation_presses = Vec::new();
 
-    loop {
+    while !remaining_fan_in_nodes.is_empty() {
         presses += 1;
         sim.press_button();
 
-        if sim
-            .pulses
-            .iter()
-            .find(|pulse| !pulse.on && pulse.to == "rx")
-            .is_some()
-        {
-            break;
+        if let Some(pulse) = sim.pulses.iter().find(|pulse| {
+            pulse.on && pulse.to == "qn" && remaining_fan_in_nodes.contains(&pulse.from)
+        }) {
+            activation_presses.push(presses);
+            remaining_fan_in_nodes.remove(&pulse.from);
         }
 
         sim.pulses.clear();
     }
-    presses
+
+    lcm(&activation_presses)
+}
+
+fn lcm(nums: &[usize]) -> usize {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd_of_two_numbers(a, b)
+}
+
+fn gcd_of_two_numbers(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
 }
 
 #[cfg(test)]
