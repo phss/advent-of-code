29b837cc88b70ccf4d7379bf23e12300fb321f3a
Paulo Schneider <paulo.schneider@gmail.com>
2025-02-15 09:42:57 +0000

    Slow solution for day 21 part 2

--- 2023/rust/src/day21.rs
+++ 2023/rust/src/day21.rs
@@ -1,3 +1,4 @@
+use core::net;
 use std::collections::HashSet;
 
 use crate::parser;
@@ -9,12 +10,15 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-    0
+    let lines: Vec<String> = parser::read("data/day21.txt").unwrap();
+    let map = parse(&lines);
+    infinite_plots_after_steps(&map, 131 * 3)
+    // infinite_plots_after_steps(&map, 26501365)
 }
 
 fn plots_after_steps(map: &Vec<Vec<char>>, steps: usize) -> usize {
-    let width = map[0].len();
-    let height = map.len();
+    let width = map[0].len() as isize;
+    let height = map.len() as isize;
 
     let start = position(map, 'S');
     let mut visited = HashSet::new();
@@ -26,17 +30,17 @@ fn plots_after_steps(map: &Vec<Vec<char>>, steps: usize) -> usize {
 
         for (x, y) in visited {
             for (dir_x, dir_y) in directions {
-                let next_x = x.checked_add_signed(dir_x).filter(|n| *n < width);
-                let next_y = y.checked_add_signed(dir_y).filter(|n| *n < height);
-                if next_x.is_none() || next_y.is_none() {
-                    continue;
-                }
-
-                let next_x = next_x.unwrap();
-                let next_y = next_y.unwrap();
+                let next_x = x + dir_x;
+                let next_y = y + dir_y;
                 let next_position = (next_x, next_y);
 
-                if new_visited.contains(&next_position) || map[next_y][next_x] == '#' {
+                if new_visited.contains(&next_position)
+                    || next_x < 0
+                    || next_x >= width
+                    || next_y < 0
+                    || next_y >= height
+                    || map[next_y as usize][next_x as usize] == '#'
+                {
                     continue;
                 }
 
@@ -50,15 +54,50 @@ fn plots_after_steps(map: &Vec<Vec<char>>, steps: usize) -> usize {
     visited.len()
 }
 
+fn infinite_plots_after_steps(map: &Vec<Vec<char>>, steps: usize) -> usize {
+    let width = map[0].len() as isize;
+    let height = map.len() as isize;
+
+    let start = position(map, 'S');
+    let mut visited = HashSet::new();
+    visited.insert(start);
+
+    let directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    for _ in 0..steps {
+        let mut new_visited = HashSet::new();
+
+        for (x, y) in visited {
+            for (dir_x, dir_y) in directions {
+                let next_x = x + dir_x;
+                let next_y = y + dir_y;
+                let next_position = (next_x, next_y);
+                let adjusted_x = next_x.rem_euclid(width) as usize;
+                let adjusted_y = next_y.rem_euclid(height) as usize;
+
+                if new_visited.contains(&next_position) || map[adjusted_y][adjusted_x] == '#' {
+                    continue;
+                }
+
+                new_visited.insert(next_position);
+            }
+        }
+
+        visited = new_visited;
+        // println!("{}", visited.len());
+    }
+
+    visited.len()
+}
+
 fn parse(lines: &Vec<String>) -> Vec<Vec<char>> {
     lines.iter().map(|s| s.chars().collect()).collect()
 }
 
-fn position(map: &Vec<Vec<char>>, char: char) -> (usize, usize) {
+fn position(map: &Vec<Vec<char>>, char: char) -> (isize, isize) {
     let mut position = (0, 0);
     for (y, row) in map.iter().enumerate() {
         if let Some(x) = row.iter().position(|&c| c == char) {
-            position = (x, y);
+            position = (x as isize, y as isize);
             break;
         }
     }
@@ -93,5 +132,25 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let lines = vec![
+            "...........",
+            ".....###.#.",
+            ".###.##..#.",
+            "..#.#...#..",
+            "....#.#....",
+            ".##..S####.",
+            ".##..#...#.",
+            ".......##..",
+            ".##.#.####.",
+            ".##..##.##.",
+            "...........",
+        ];
+        let lines: Vec<String> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
+        let map = parse(&lines);
+
+        let result = infinite_plots_after_steps(&map, 100);
+
+        assert_eq!(result, 6536);
+    }
 }
