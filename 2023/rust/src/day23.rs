ba08bd0c1cccfb1fc3d7e9a9109ed7990d5c5849
Paulo Schneider <paulo.schneider@gmail.com>
2025-02-23 15:29:50 +0000

    Slow solution for day 23 part 2

--- 2023/rust/src/day23.rs
+++ 2023/rust/src/day23.rs
@@ -9,7 +9,9 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-    0
+    let lines: Vec<String> = parser::read("data/day23.txt").unwrap();
+    let map = parse(&lines);
+    longest_hike_no_slopes(&map, HashSet::new(), (1, 0), (139, 140))
 }
 
 fn longest_hike(
@@ -40,6 +42,44 @@ fn longest_hike(
         }
 
         max_steps = max_steps.max(longest_hike(map, new_visited.clone(), next_position, to));
+    }
+
+    max_steps
+}
+
+fn longest_hike_no_slopes(
+    map: &Vec<Vec<char>>,
+    visited: HashSet<(usize, usize)>,
+    from: (usize, usize),
+    to: (usize, usize),
+) -> usize {
+    if from == to {
+        return visited.len();
+    }
+
+    let mut new_visited = visited.clone();
+    new_visited.insert(from);
+
+    let (x, y) = from;
+    let mut max_steps = 0;
+
+    let directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    for (dir_x, dir_y) in directions {
+        let next_x = x.checked_add_signed(dir_x).unwrap_or(0);
+        let next_y = y.checked_add_signed(dir_y).unwrap_or(0);
+        let next_position = (next_x, next_y);
+        let next_char = map[next_y][next_x];
+
+        if new_visited.contains(&next_position) || next_char == '#' {
+            continue;
+        }
+
+        max_steps = max_steps.max(longest_hike_no_slopes(
+            map,
+            new_visited.clone(),
+            next_position,
+            to,
+        ));
     }
 
     max_steps
@@ -89,5 +129,37 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let lines = vec![
+            "#.#####################",
+            "#.......#########...###",
+            "#######.#########.#.###",
+            "###.....#.>.>.###.#.###",
+            "###v#####.#v#.###.#.###",
+            "###.>...#.#.#.....#...#",
+            "###v###.#.#.#########.#",
+            "###...#.#.#.......#...#",
+            "#####.#.#.#######.#.###",
+            "#.....#.#.#.......#...#",
+            "#.#####.#.#.#########v#",
+            "#.#...#...#...###...>.#",
+            "#.#.#v#######v###.###v#",
+            "#...#.>.#...>.>.#.###.#",
+            "#####v#.#.###v#.#.###.#",
+            "#.....#...#...#.#.#...#",
+            "#.#########.###.#.#.###",
+            "#...###...#...#...#.###",
+            "###.###.#.###v#####v###",
+            "#...#...#.#.>.>.#.>.###",
+            "#.###.###.#.###.#.#v###",
+            "#.....###...###...#...#",
+            "#####################.#",
+        ];
+        let lines: Vec<String> = lines.into_iter().map(|s| s.parse().unwrap()).collect();
+        let map = parse(&lines);
+
+        let result = longest_hike_no_slopes(&map, HashSet::new(), (1, 0), (21, 22));
+
+        assert_eq!(result, 154);
+    }
 }
