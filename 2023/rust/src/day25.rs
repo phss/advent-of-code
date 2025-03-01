c586288f2303598dfa09822143db4151f7ded3c4
Paulo Schneider <paulo.schneider@gmail.com>
2025-03-01 09:40:34 +0000

    Setup for day 25

---
+++ 2023/rust/src/day25.rs
@@ -0,0 +1,20 @@
+use crate::parser;
+
+pub fn part1() -> usize {
+    0
+}
+
+pub fn part2() -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_input_part_1() {}
+
+    #[test]
+    fn sample_input_part_2() {}
+}
