c8447c41f921d9708b0ba76defc801f2df992fc9
Paulo Schneider <paulo.schneider@gmail.com>
2025-03-09 11:35:43 +0000

    Calculate 3d directions from 2d direction

--- 2022/rust/src/day18.rs
+++ 2022/rust/src/day18.rs
@@ -9,7 +9,9 @@ pub fn part1() -> usize {
 }
 
 pub fn part2() -> usize {
-    0
+    let lines: Vec<String> = parser::read("data/day18.txt").unwrap();
+    let cubes = parse(lines);
+    outside_surface_area(&cubes)
 }
 
 fn surface_area(cubes: &Vec<(usize, usize, usize)>) -> usize {
@@ -44,6 +46,30 @@ fn surface_area(cubes: &Vec<(usize, usize, usize)>) -> usize {
     area
 }
 
+fn outside_surface_area(cubes: &Vec<(usize, usize, usize)>) -> usize {
+    let cubes: HashSet<_> = cubes.iter().cloned().collect();
+    let visited: HashSet<usize> = HashSet::new();
+
+    let start = cubes.iter().min().unwrap();
+    let orientation = (0, 0, -1);
+
+    visited.len()
+}
+
+fn to_3d_direction(
+    orientation: &(isize, isize, isize),
+    direction: &(isize, isize),
+) -> (isize, isize, isize) {
+    let (x_ori, y_ori, z_ori) = orientation.clone();
+    let (x_2d_dir, y_2d_dir) = direction.clone();
+
+    (
+        x_2d_dir * z_ori.abs() + x_2d_dir * y_ori.abs(),
+        y_2d_dir * z_ori.abs() + y_2d_dir * x_ori.abs(),
+        x_2d_dir * x_ori.abs() + y_2d_dir * y_ori.abs(),
+    )
+}
+
 fn parse(input: Vec<String>) -> Vec<(usize, usize, usize)> {
     input
         .into_iter()
@@ -56,6 +82,8 @@ fn parse(input: Vec<String>) -> Vec<(usize, usize, usize)> {
 
 #[cfg(test)]
 mod tests {
+    use std::ops::Neg;
+
     use super::*;
 
     #[test]
@@ -73,5 +101,60 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let input = vec![
+            "2,2,2", "1,2,2", "3,2,2", "2,1,2", "2,3,2", "2,2,1", "2,2,3", "2,2,4", "2,2,6",
+            "1,2,5", "3,2,5", "2,1,5", "2,3,5",
+        ];
+        let input = input.iter().map(|s| s.to_string()).collect();
+        let cubes = parse(input);
+
+        let result = outside_surface_area(&cubes);
+
+        assert_eq!(result, 58);
+    }
+
+    #[test]
+    fn test_to_3d_direction() {
+        let left = (-1, 0);
+        let right = (1, 0);
+        let up = (0, -1);
+        let down = (0, 1);
+
+        let top = (0, 0, -1);
+        assert_eq!(to_3d_direction(&top, &left), (-1, 0, 0));
+        assert_eq!(to_3d_direction(&top, &right), (1, 0, 0));
+        assert_eq!(to_3d_direction(&top, &up), (0, -1, 0));
+        assert_eq!(to_3d_direction(&top, &down), (0, 1, 0));
+
+        let bottom = (0, 0, 1);
+        assert_eq!(to_3d_direction(&bottom, &left), (-1, 0, 0));
+        assert_eq!(to_3d_direction(&bottom, &right), (1, 0, 0));
+        assert_eq!(to_3d_direction(&bottom, &up), (0, -1, 0));
+        assert_eq!(to_3d_direction(&bottom, &down), (0, 1, 0));
+
+        let east = (-1, 0, 0);
+        assert_eq!(to_3d_direction(&east, &left), (0, 0, -1));
+        assert_eq!(to_3d_direction(&east, &right), (0, 0, 1));
+        assert_eq!(to_3d_direction(&east, &up), (0, -1, 0));
+        assert_eq!(to_3d_direction(&east, &down), (0, 1, 0));
+
+        let west = (1, 0, 0);
+        assert_eq!(to_3d_direction(&west, &left), (0, 0, -1));
+        assert_eq!(to_3d_direction(&west, &right), (0, 0, 1));
+        assert_eq!(to_3d_direction(&west, &up), (0, -1, 0));
+        assert_eq!(to_3d_direction(&west, &down), (0, 1, 0));
+
+        let north = (0, -1, 0);
+        assert_eq!(to_3d_direction(&north, &left), (-1, 0, 0));
+        assert_eq!(to_3d_direction(&north, &right), (1, 0, 0));
+        assert_eq!(to_3d_direction(&north, &up), (0, 0, -1));
+        assert_eq!(to_3d_direction(&north, &down), (0, 0, 1));
+
+        let south = (0, 1, 0);
+        assert_eq!(to_3d_direction(&south, &left), (-1, 0, 0));
+        assert_eq!(to_3d_direction(&south, &right), (1, 0, 0));
+        assert_eq!(to_3d_direction(&south, &up), (0, 0, -1));
+        assert_eq!(to_3d_direction(&south, &down), (0, 0, 1));
+    }
 }
