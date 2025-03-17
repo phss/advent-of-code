1cf17ba3f05199e7826042b195da45e620de1240
Paulo Schneider <paulo.schneider@gmail.com>
2025-03-17 14:27:59 +0000

    Solution for day 20 part 2

--- 2022/rust/src/day20.rs
+++ 2022/rust/src/day20.rs
@@ -2,33 +2,42 @@ use crate::parser;
 
 pub fn part1() -> usize {
     let numbers: Vec<isize> = parser::read("data/day20.txt").unwrap();
-    sum_after_mixing(&numbers)
+    sum_after_mixing(&numbers, 1, 1)
 }
 
 pub fn part2() -> usize {
-    0
+    let numbers: Vec<isize> = parser::read("data/day20.txt").unwrap();
+    sum_after_mixing(&numbers, 811589153, 10)
 }
 
-fn sum_after_mixing(numbers: &Vec<isize>) -> usize {
-    let mut mixed: Vec<(usize, &isize)> = numbers.iter().enumerate().clone().collect();
+fn sum_after_mixing(numbers: &Vec<isize>, decryption_key: usize, times: usize) -> usize {
+    let mut mixed: Vec<(usize, isize)> = numbers
+        .iter()
+        .map(|n| n * decryption_key as isize)
+        .enumerate()
+        .clone()
+        .collect();
 
-    for (original_index, number) in numbers.iter().enumerate() {
-        let mix_from = mixed
-            .iter()
-            .position(|(i, _)| *i == original_index)
-            .unwrap();
+    for _ in 0..times {
+        for original_index in 0..mixed.len() {
+            let mix_from = mixed
+                .iter()
+                .position(|(i, _)| *i == original_index)
+                .unwrap();
 
-        let mut mix_to = mix_from as isize + number;
-        mix_to = mix_to.rem_euclid(mixed.len() as isize - 1);
+            let elem = mixed.remove(mix_from);
 
-        let elem = mixed.remove(mix_from);
-        mixed.insert(mix_to as usize, elem);
+            let mut mix_to = mix_from as isize + elem.1;
+            mix_to = mix_to.rem_euclid(mixed.len() as isize);
+
+            mixed.insert(mix_to as usize, elem);
+        }
     }
 
-    let zero_pos = mixed.iter().position(|(_, n)| **n == 0).unwrap();
-    let a = (zero_pos + 1000).rem_euclid(numbers.len());
-    let b = (zero_pos + 2000).rem_euclid(numbers.len());
-    let c = (zero_pos + 3000).rem_euclid(numbers.len());
+    let zero_pos = mixed.iter().position(|(_, n)| *n == 0).unwrap();
+    let a = (zero_pos + 1000).rem_euclid(mixed.len());
+    let b = (zero_pos + 2000).rem_euclid(mixed.len());
+    let c = (zero_pos + 3000).rem_euclid(mixed.len());
 
     (mixed[a].1 + mixed[b].1 + mixed[c].1) as usize
 }
@@ -42,11 +51,18 @@ mod tests {
         let input = vec!["1", "2", "-3", "3", "-2", "0", "4"];
         let numbers: Vec<isize> = input.iter().map(|s| s.parse().unwrap()).collect();
 
-        let result = sum_after_mixing(&numbers);
+        let result = sum_after_mixing(&numbers, 1, 1);
 
         assert_eq!(result, 3);
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let input = vec!["1", "2", "-3", "3", "-2", "0", "4"];
+        let numbers: Vec<isize> = input.iter().map(|s| s.parse().unwrap()).collect();
+
+        let result = sum_after_mixing(&numbers, 811589153, 10);
+
+        assert_eq!(result, 1623178306);
+    }
 }
