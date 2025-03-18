e04b29dbbc486248bd94c1c849c2f76dde6db8d6
Paulo Schneider <paulo.schneider@gmail.com>
2025-03-18 18:19:46 +0000

    Setup for part 2

--- 2022/rust/src/day21.rs
+++ 2022/rust/src/day21.rs
@@ -99,5 +99,29 @@ mod tests {
     }
 
     #[test]
-    fn sample_input_part_2() {}
+    fn sample_input_part_2() {
+        let input = vec![
+            "root: pppw + sjmn",
+            "dbpl: 5",
+            "cczh: sllz + lgvd",
+            "zczc: 2",
+            "ptdq: humn - dvpt",
+            "dvpt: 3",
+            "lfqf: 4",
+            "humn: 5",
+            "ljgn: 2",
+            "sjmn: drzm * dbpl",
+            "sllz: 4",
+            "pppw: cczh / lfqf",
+            "lgvd: ljgn * ptdq",
+            "drzm: hmdt - zczc",
+            "hmdt: 32",
+        ];
+        let lines: Vec<String> = input.iter().map(|s| s.parse().unwrap()).collect();
+        let monkeys = parse(lines);
+
+        let result = root_equality_number(&monkeys);
+
+        assert_eq!(result, 301);
+    }
 }
