825cb35644d4142bc62c216401327ed7fd78e868
Paulo Schneider <paulo.schneider@gmail.com>
2025-03-26 10:04:12 +0000

    Setup for day 23

--- 2022/rust/src/main.rs
+++ 2022/rust/src/main.rs
@@ -1,6 +1,6 @@
 use advent_of_code::{
     day1, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day2, day20, day21,
-    day22, day3, day4, day5, day6, day7, day8, day9,
+    day22, day23, day3, day4, day5, day6, day7, day8, day9,
 };
 use std::env;
 
@@ -63,6 +63,8 @@ fn run(day: usize, part: usize) -> usize {
         (21, 2) => day21::part2,
         (22, 1) => day22::part1,
         (22, 2) => day22::part2,
+        (23, 1) => day23::part1,
+        (23, 2) => day23::part2,
         _ => panic!("No solution yet for day {} and part {}", day, part),
     };
     solution()
