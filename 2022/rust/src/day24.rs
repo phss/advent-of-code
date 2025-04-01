c21ef631fbc2c2ad1a40e5a5bf64219f59f9d355
Paulo Schneider <paulo.schneider@gmail.com>
2025-04-01 09:29:33 +0100

    Parse input

--- 2022/rust/src/day24.rs
+++ 2022/rust/src/day24.rs
@@ -1,4 +1,8 @@
+use std::collections::HashMap;
+
 use crate::parser;
+
+type position = (usize, usize);
 
 pub fn part1() -> usize {
     0
@@ -8,12 +12,40 @@ pub fn part2() -> usize {
     0
 }
 
+fn parse(lines: Vec<String>) -> (position, position, HashMap<position, Vec<char>>) {
+    let start = (1, 0);
+    let end = (lines.len() - 1, lines[0].len() - 2);
+    let mut blizzards = HashMap::new();
+
+    for (y, line) in lines.iter().enumerate() {
+        for (x, cell) in line.chars().enumerate() {
+            if cell != '.' && cell != '#' {
+                blizzards.entry((x, y)).or_insert_with(Vec::new).push(cell);
+            }
+        }
+    }
+
+    (start, end, blizzards)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn sample_input_part_1() {}
+    fn sample_input_part_1() {
+        let input = vec![
+            "#.######", "#>>.<^<#", "#.<..<<#", "#>v.><>#", "#<^v^^>#", "######.#",
+        ];
+        let lines: Vec<String> = input.iter().map(|s| s.parse().unwrap()).collect();
+        let (start, end, blizzards) = parse(lines);
+
+        println!("{:?}", blizzards);
+
+        // let result = min_steps(start, end, blizzards);
+
+        // assert_eq!(result, 18);
+    }
 
     #[test]
     fn sample_input_part_2() {}
