06c5b47d1ff7a25438ba04a4b0dc336b07fcb43d
Paulo Schneider <paulo.schneider@gmail.com>
2025-11-19 13:36:33 +0000

    Implement challenge runner

--- 2025/python/main.py
+++ 2025/python/main.py
@@ -1,5 +1,25 @@
+import argparse
+
+
+def run_challenge(challenge: str, part: int):
+    challenge = __import__(f"challenges.{challenge}", fromlist=["part1", "part2"])
+
+    match part:
+        case 1:
+            challenge.part1()
+        case 2:
+            challenge.part2()
+        case _:
+            print("No such part")
+
+
 def main():
-    print("Hello from python!")
+    parser = argparse.ArgumentParser(prog="Advent of Code 2025")
+    parser.add_argument("challenge")
+    parser.add_argument("--part", type=int)
+    args = parser.parse_args()
+
+    run_challenge(args.challenge, args.part)
 
 
 if __name__ == "__main__":
