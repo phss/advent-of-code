21289c1c15752100225b1409c5c794eeddd5fadc
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-01 10:36:04 +0000

    Solution for day 1 part 2

--- 2025/python/challenges/day1.py
+++ 2025/python/challenges/day1.py
@@ -1,11 +1,9 @@
 def part1(lines: list[str]) -> int:
+    rotations = __parse_input(lines)
     password = 0
 
     dial_location = 50
-    for line in lines:
-        direction = line[0]
-        clicks = int(line[1:])
-
+    for direction, clicks in rotations:
         match direction:
             case "R":
                 dial_location += clicks
@@ -18,5 +16,35 @@ def part1(lines: list[str]) -> int:
     return password
 
 
-def part2() -> int:
-    None
+def part2(lines: list[str]) -> int:
+    rotations = __parse_input(lines)
+    password = 0
+
+    dial_location = 50
+    for direction, clicks in rotations:
+        password += clicks // 100
+        clicks = clicks % 100
+
+        match direction:
+            case "R":
+                next_dial_location = dial_location + clicks
+            case "L":
+                next_dial_location = dial_location - clicks
+
+        crossed_zero = next_dial_location <= 0 or next_dial_location >= 100
+        if dial_location != 0 and crossed_zero:
+            password += 1
+
+        dial_location = next_dial_location % 100
+
+    return password
+
+
+def __parse_input(lines: list[str]) -> list[(str, int)]:
+    rotations = []
+    for line in lines:
+        direction = line[0]
+        clicks = int(line[1:])
+        rotations.append((direction, clicks))
+
+    return rotations
--- 2025/python/tests/test_day1.py
+++ 2025/python/tests/test_day1.py
@@ -21,6 +21,26 @@ class TestTemplate:
         assert result == 3
 
     def test_part2(self):
-        result = challenge.part2()
+        result = challenge.part2(self.lines)
 
-        assert result is None
+        assert result == 6
+
+    def test_part2_mega_rotation(self):
+        lines = [
+            "L68",
+            "L30",
+            "R48",
+            "L5",
+            "R60",
+            "R1100",
+            "L55",
+            "L1",
+            "L99",
+            "L1000",
+            "R14",
+            "L82",
+        ]
+
+        result = challenge.part2(lines)
+
+        assert result == 27
