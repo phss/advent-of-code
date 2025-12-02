479d1ef5bd42870190d04c2f174e57b39a72bf99
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-02 10:49:54 +0000

    Solution for day 2 part 1

--- 2025/python/challenges/day2.py
+++ 2025/python/challenges/day2.py
@@ -1,6 +1,29 @@
 def part1(lines: list[str]) -> int:
-    None
+    id_ranges = __parse_input(lines)
+    sum_invalid_ids = 0
+
+    for i in range(1, 100000):
+        i_str = str(i)
+        invalid_id = int(i_str + i_str)
+
+        for id_range in id_ranges:
+            if invalid_id in id_range:
+                sum_invalid_ids += invalid_id
+
+    return sum_invalid_ids
 
 
 def part2(lines: list[str]) -> int:
     None
+
+
+def __parse_input(lines: list[str]) -> list[range[int]]:
+    entries = lines[0].split(",")
+    id_ranges = []
+
+    for entry in entries:
+        a, b = entry.split("-")
+        id_range = range(int(a), int(b) + 1)
+        id_ranges.append(id_range)
+
+    return id_ranges
--- 2025/python/tests/test_day2.py
+++ 2025/python/tests/test_day2.py
@@ -2,12 +2,14 @@ import challenges.day2 as challenge
 
 
 class TestTemplate:
-    lines = []
+    lines = [
+        "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    ]
 
     def test_part1(self):
         result = challenge.part1(self.lines)
 
-        assert result is None
+        assert result == 1227775554
 
     def test_part2(self):
         result = challenge.part2(self.lines)
