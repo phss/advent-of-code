1be86c3e611ec26ac22b6cd99f1d7c551029f9c4
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-02 08:50:23 +0000

    Setup for day 2

---
+++ 2025/python/tests/test_day2.py
@@ -0,0 +1,15 @@
+import challenges.day2 as challenge
+
+
+class TestTemplate:
+    lines = []
+
+    def test_part1(self):
+        result = challenge.part1(self.lines)
+
+        assert result is None
+
+    def test_part2(self):
+        result = challenge.part2(self.lines)
+
+        assert result is None
