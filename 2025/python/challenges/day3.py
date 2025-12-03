9b79de6718e526dea415c02698f9df0e8dccac9e
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-03 09:47:55 +0000

    Cleanup solution and remove unused parse method

--- 2025/python/challenges/day3.py
+++ 2025/python/challenges/day3.py
@@ -16,27 +16,19 @@ def __calculate_total_output_joltage(
         last_battery_index = -1
 
         for end_buffer in reversed(range(num_batteries)):
+            start_index = last_battery_index + 1
+            end_index = len(batteries) - end_buffer
+            iteration_battery_index = 0
             highest_battery = "0"
-            blah = 0
-            for i, battery in enumerate(
-                batteries[(last_battery_index + 1) : (len(batteries) - end_buffer)]
-            ):
+
+            for i, battery in enumerate(batteries[start_index:end_index]):
                 if battery > highest_battery:
                     highest_battery = battery
-                    blah = last_battery_index + 1 + i
-            last_battery_index = blah
+                    iteration_battery_index = start_index + i
+
+            last_battery_index = iteration_battery_index
             battery_str += highest_battery
 
         total_output_joltage += int(battery_str)
 
     return total_output_joltage
-
-
-def __parse_input(lines: list[str]) -> list[list[int]]:
-    battery_banks = []
-
-    for line in lines:
-        batteries = list(map(int, line))
-        battery_banks.append(batteries)
-
-    return battery_banks
