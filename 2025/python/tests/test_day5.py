c20cea934615719f913afd9abb4cacbde3b68641
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-05 08:31:30 +0000

    Setup for day 5

---
+++ 2025/python/tests/test_day5.py
@@ -0,0 +1,27 @@
+import challenges.day5 as challenge
+
+
+class TestChallenge:
+    lines = [
+        "3-5",
+        "10-14",
+        "16-20",
+        "12-18",
+        "",
+        "1",
+        "5",
+        "8",
+        "11",
+        "17",
+        "32",
+    ]
+
+    def test_part1(self):
+        result = challenge.part1(self.lines)
+
+        assert result is None
+
+    def test_part2(self):
+        result = challenge.part2(self.lines)
+
+        assert result is None
