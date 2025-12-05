cd16625e299183c6926601a4652c4f17463920b9
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-05 08:47:25 +0000

    Solution for day 5, part 1

--- 2025/python/challenges/day5.py
+++ 2025/python/challenges/day5.py
@@ -1,6 +1,30 @@
 def part1(lines: list[str]) -> int:
-    None
+    (fresh_ingredients, ingredients) = __parse_input(lines)
+    fresh_ingredients_count = 0
+
+    for ingredient in ingredients:
+        for fresh_ingredient_range in fresh_ingredients:
+            if ingredient in fresh_ingredient_range:
+                fresh_ingredients_count += 1
+                break
+
+    return fresh_ingredients_count
 
 
 def part2(lines: list[str]) -> int:
     None
+
+
+def __parse_input(lines: list[str]) -> tuple[list[range[int]], list[int]]:
+    separator_index = lines.index("")
+    fresh_ingredients = []
+    for line in lines[:separator_index]:
+        start, end = line.split("-")
+        ingredient_range = range(int(start), int(end) + 1)
+        fresh_ingredients.append(ingredient_range)
+
+    ingredients = []
+    for line in lines[separator_index + 1 :]:
+        ingredients.append(int(line))
+
+    return (fresh_ingredients, ingredients)
--- 2025/python/tests/test_day5.py
+++ 2025/python/tests/test_day5.py
@@ -19,7 +19,7 @@ class TestChallenge:
     def test_part1(self):
         result = challenge.part1(self.lines)
 
-        assert result is None
+        assert result == 3
 
     def test_part2(self):
         result = challenge.part2(self.lines)
