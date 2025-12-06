0bbdd9ca7e5372791f0728c9e88e469d2cf33641
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-06 09:31:54 +0000

    Solution for day 6 part 1

--- 2025/python/challenges/day6.py
+++ 2025/python/challenges/day6.py
@@ -1,6 +1,34 @@
+import re
+
+
 def part1(lines: list[str]) -> int:
-    None
+    (all_numbers, operations) = __parse_input(lines)
+    sum_of_answers = 0
+
+    for i, operation in enumerate(operations):
+        answer = all_numbers[0][i]
+        for numbers in all_numbers[1:]:
+            match operation:
+                case "+":
+                    answer += numbers[i]
+                case "*":
+                    answer *= numbers[i]
+
+        sum_of_answers += answer
+
+    return sum_of_answers
 
 
 def part2(lines: list[str]) -> int:
     None
+
+
+def __parse_input(lines: list[str]) -> list:
+    all_numbers = [list(map(int, __list_from(line))) for line in lines[:-1]]
+    operations = __list_from(lines[-1])
+    return (all_numbers, operations)
+
+
+def __list_from(line: str) -> list:
+    sanitised_string = re.sub(r"\s+", " ", line.strip())
+    return sanitised_string.split(" ")
--- 2025/python/tests/test_day6.py
+++ 2025/python/tests/test_day6.py
@@ -5,14 +5,14 @@ class TestChallenge:
     lines = [
         "123 328  51 64 ",
         " 45 64  387 23 ",
-        "    98  215 314",
+        "  6 98  215 314",
         "*   +   *   +  ",
     ]
 
     def test_part1(self):
         result = challenge.part1(self.lines)
 
-        assert result is None
+        assert result == 4277556
 
     def test_part2(self):
         result = challenge.part2(self.lines)
