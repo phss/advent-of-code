aaf38966ab2ad4f18fe8468ee85009c92e969ac5
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-07 09:35:56 +0000

    Solution for day 7 part 2

--- 2025/python/challenges/day7.py
+++ 2025/python/challenges/day7.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def part1(map: list[str]) -> int:
     split_count = 0
     beams = set()
@@ -19,7 +22,26 @@ def part1(map: list[str]) -> int:
 
 
 def part2(map: list[str]) -> int:
-    None
+    timelines = 1
+    x, y = __find_start(map)
+    beams = set()
+    beams.add((x, y, timelines))
+
+    for row in map[1:]:
+        new_beams = Counter()
+        for x, y, t in beams:
+            if row[x] == "^":
+                timelines += t
+                new_beams[(x - 1, y + 1)] += t
+                new_beams[(x + 1, y + 1)] += t
+            else:
+                new_beams[(x, y + 1)] += t
+
+        beams = set()
+        for (x, y), t in new_beams.items():
+            beams.add((x, y, t))
+
+    return timelines
 
 
 def __find_start(map: list[str]) -> tuple[int, int]:
