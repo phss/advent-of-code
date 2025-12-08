18eb9fd5b17c35df1bc89530faa9cad3a41ee208
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-08 09:26:36 +0000

    Solution for day 8 part 1

--- 2025/python/challenges/day8.py
+++ 2025/python/challenges/day8.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from heapq import heappop, heappush
 from math import sqrt
 
@@ -15,8 +16,24 @@ def mult_three_largest_circuits(
     boxes: list[tuple[int, int, int]], num_connects: int
 ) -> int:
     connections = list_shortest_connections(boxes)
+
+    circuits_map = {}
+    for i, box in enumerate(boxes):
+        circuits_map[box] = i
+
     for _ in range(num_connects):
-        print(heappop(connections))
+        _, (a, b) = heappop(connections)
+        a_circuit = circuits_map[a]
+        b_circuit = circuits_map[b]
+        for box, circuit in circuits_map.items():
+            if circuit == b_circuit:
+                circuits_map[box] = a_circuit
+
+    counter = Counter(circuits_map.values())
+    result = 1
+    for _, size in counter.most_common(3):
+        result *= size
+    return result
 
 
 def list_shortest_connections(boxes: list[tuple[int, int, int]]) -> list:
