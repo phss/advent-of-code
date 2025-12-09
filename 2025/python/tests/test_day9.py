74a10b75f4ae0532b95543cac2e63ed5295b9950
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-09 08:23:20 +0000

    Setup for day 9

---
+++ 2025/python/tests/test_day9.py
@@ -0,0 +1,24 @@
+import challenges.day9 as challenge
+
+
+class TestChallenge:
+    lines = [
+        "7,1",
+        "11,1",
+        "11,7",
+        "9,7",
+        "9,5",
+        "2,5",
+        "2,3",
+        "7,3",
+    ]
+
+    def test_part1(self):
+        result = challenge.part1(self.lines)
+
+        assert result is None
+
+    def test_part2(self):
+        result = challenge.part2(self.lines)
+
+        assert result is None
