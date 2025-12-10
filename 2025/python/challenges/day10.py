3e4ee6080bae9a5fd92a666a744ff8798a2daa74
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-10 09:15:45 +0000

    Slow solution for day 10 part 1

--- 2025/python/challenges/day10.py
+++ 2025/python/challenges/day10.py
@@ -1,15 +1,40 @@
+from heapq import heappop, heappush
 import re
 
 
 def part1(lines: list[str]) -> int:
     machines = __parse_input(lines)
 
+    total_button_presses = 0
     for machine in machines:
-        print(machine)
+        lights, buttons, _ = machine
+        total_button_presses += __min_button_presses(lights, buttons)
+        print(machine, total_button_presses)
+
+    return total_button_presses
 
 
 def part2(lines: list[str]) -> int:
     None
+
+
+def __min_button_presses(end_lights: str, buttons: list[list[int]]) -> int:
+    search = []
+    heappush(search, (0, len(end_lights) * "."))
+
+    while True:
+        presses, lights = heappop(search)  # noqa: F821
+        if lights == end_lights:
+            return presses
+
+        for button in buttons:
+            lights_after_button = list(lights)
+            for i in button:
+                if lights_after_button[i] == ".":
+                    lights_after_button[i] = "#"
+                else:
+                    lights_after_button[i] = "."
+            heappush(search, (presses + 1, "".join(lights_after_button)))
 
 
 def __parse_input(lines: list[str]) -> list:
