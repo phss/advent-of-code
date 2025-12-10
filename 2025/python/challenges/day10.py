31ea38f71145c6318c2bcce33b62cd0330b78f54
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-10 12:07:29 +0000

    Solution for day 10 part 2
    Had to learn Z3.
    
    Heavily inspired by:
    - https://www.reddit.com/r/adventofcode/comments/1pity70/comment/nt9ycnn/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
    - https://www.reddit.com/r/adventofcode/comments/1pity70/comment/nt99z1y/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button

--- 2025/python/challenges/day10.py
+++ 2025/python/challenges/day10.py
@@ -1,6 +1,8 @@
 from collections import deque
 import re
 
+import z3
+
 
 def part1(lines: list[str]) -> int:
     machines = __parse_input(lines)
@@ -8,16 +10,23 @@ def part1(lines: list[str]) -> int:
     total_button_presses = 0
     for machine in machines:
         lights, buttons, _ = machine
-        total_button_presses += __min_button_presses(lights, buttons)
+        total_button_presses += __min_button_presses_for_lights(lights, buttons)
 
     return total_button_presses
 
 
 def part2(lines: list[str]) -> int:
-    None
+    machines = __parse_input(lines)
+
+    total_button_presses = 0
+    for machine in machines:
+        _, buttons, joltage = machine
+        total_button_presses += __min_button_presses_for_joltage(joltage, buttons)
+
+    return total_button_presses
 
 
-def __min_button_presses(end_lights: str, buttons: list[list[int]]) -> int:
+def __min_button_presses_for_lights(end_lights: str, buttons: list[list[int]]) -> int:
     search = deque()
     search.append((0, len(end_lights) * "."))
 
@@ -34,6 +43,31 @@ def __min_button_presses(end_lights: str, buttons: list[list[int]]) -> int:
                 else:
                     lights_after_button[i] = "."
             search.append((presses + 1, "".join(lights_after_button)))
+
+
+def __min_button_presses_for_joltage(
+    end_joltage: list[int], buttons: list[list[int]]
+) -> int:
+    solver = z3.Optimize()
+    variables = [z3.Int(f"x{i}") for i, _ in enumerate(buttons)]
+
+    for variable in variables:
+        solver.add(variable >= 0)
+
+    for joltage_index, joltage in enumerate(end_joltage):
+        relevant_variables = [
+            variable
+            for variable, button_indexes in zip(variables, buttons)
+            if joltage_index in button_indexes
+        ]
+
+        solver.add(joltage == z3.Sum(relevant_variables))
+
+    total_presses = solver.minimize(sum(variables))
+    if solver.check() == z3.sat:
+        return total_presses.value().as_long()
+    else:
+        raise "couldn't solve it"
 
 
 def __parse_input(lines: list[str]) -> list:
