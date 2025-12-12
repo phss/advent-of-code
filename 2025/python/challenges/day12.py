7d1723495fee873f4a4ff0840680f29e5eed45bd
Paulo Schneider <paulo.schneider@gmail.com>
2025-12-12 08:05:35 +0000

    Setup for day 12

---
+++ 2025/python/challenges/day12.py
@@ -0,0 +1,6 @@
+def part1(lines: list[str]) -> int:
+    None
+
+
+def part2(lines: list[str]) -> int:
+    None
---
+++ 2025/python/tests/test_day12.py
@@ -0,0 +1,49 @@
+import challenges.day12 as challenge
+
+
+class TestChallenge:
+    lines = [
+        "0:",
+        "###",
+        "##.",
+        "##.",
+        "",
+        "1:",
+        "###",
+        "##.",
+        ".##",
+        "",
+        "2:",
+        ".##",
+        "###",
+        "##.",
+        "",
+        "3:",
+        "##.",
+        "###",
+        "##.",
+        "",
+        "4:",
+        "###",
+        "#..",
+        "###",
+        "",
+        "5:",
+        "###",
+        ".#.",
+        "###",
+        "",
+        "4x4: 0 0 0 0 2 0",
+        "12x5: 1 0 1 0 2 2",
+        "12x5: 1 0 1 0 3 2",
+    ]
+
+    def test_part1(self):
+        result = challenge.part1(self.lines)
+
+        assert result is None
+
+    def test_part2(self):
+        result = challenge.part2(self.lines)
+
+        assert result is None
